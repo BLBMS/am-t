57067e23d3d0cb7335ae84ff19a654d3c9b0c5db
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-10-03 20:58:36 +0200

    Update nastavi-cc-ssh.sh

--- 0/nastavi-cc-ssh.sh
+++ 0/nastavi-cc-ssh.sh
@@ -7,42 +7,8 @@
 #->  cd ~/ && rm -f ~/nastavi-cc-ssh.sh && wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/nastavi-cc-ssh.sh && chmod +x nastavi-cc-ssh.sh && ~/nastavi-cc-ssh.sh
 
 # se izvaja samo
-echo -e "\n\e[93mnastavljam TERMUX\e[0m\n"
-pkg install -y openssh net-tools nano 
-echo -e "\n\e[93mnastavljam key\e[0m\n"
-echo -e "\n\e[93mpreveri IP !!\e[0m\n"
-cd ~/
-rm -rf ~/.ssh/
-mkdir ~/.ssh
-chmod 0700 ~/.ssh
-cat << EOF > ~/.ssh/authorized_keys
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAe7mHnisRNUXZ8u5AaeKxm7/ixbaacLWk6S6bpqlEom blb@blb
-EOF
-chmod 0600 ~/.ssh/authorized_keys
-echo -e "\n\e[93mnastavljam SSH\e[0m\n"
-# nastavi SSH
-sshd
-#  Če je error - to nekam vnesi ???:
-#  echo "#sshd: no hostkeys available -- exiting"
-ssh-keygen -A
-#  whoami
-my_name=$(whoami)
-echo "whoami=" $my_name
-#  Ustvari password
-passwd $my_name
-# Nastavi IP
-ifconfig_out=$(ifconfig)
-ip_line=$(echo "$ifconfig_out" | grep 'inet 192')
-phone_ip=$(echo "$ip_line" | cut -d'.' -f4 | cut -c1-3)
-echo "IP=" $phone_ip
-rm -f ~/*.ip
-cat << EOF > ~/$phone_ip.ip
-EOF
-echo $$phone_ip >> ~/$phone_ip.ip
-# Nastavi SSH
-echo "■■■■ update to blb ssh ■■■■"
-ssh $my_name@192.168.100.$phone_ip -p 8022
-echo "■■■■ nastavitev delavca ■■■■"
+pkg install -y openssh net-tools nano screen
+echo -e "\n\e[93m■■■■ nastavitev delavca ■■■■\e[0m\n"
 cd ~/
 # Poišči če je datoteka z končnico .ww v mapi
 if ls ~/*.ww >/dev/null 2>&1; then
@@ -65,3 +31,35 @@ else
 fi
 echo -e "\n\e[93m-> Ime delavca je: "$delavec
 echo "■■■■ done ■■■■"
+echo -e "\n\e[93mnastavljam SSH\e[0m\n"
+cd ~/
+rm -rf ~/.ssh/
+mkdir ~/.ssh
+chmod 0700 ~/.ssh
+cat << EOF > ~/.ssh/authorized_keys
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAe7mHnisRNUXZ8u5AaeKxm7/ixbaacLWk6S6bpqlEom blb@blb
+EOF
+chmod 0600 ~/.ssh/authorized_keys
+# nastavi SSH
+sshd
+#  Če je error - to nekam vnesi ???:
+#  echo "#sshd: no hostkeys available -- exiting"
+ssh-keygen -A
+#  whoami
+my_name=$(whoami)
+echo "whoami=" $my_name
+#  Ustvari password
+passwd $my_name
+# Nastavi IP
+ifconfig_out=$(ifconfig)
+ip_line=$(echo "$ifconfig_out" | grep 'inet 192')
+phone_ip=$(echo "$ip_line" | cut -d'.' -f4 | cut -c1-3)
+echo "IP=" $phone_ip
+rm -f ~/*.ip
+cat << EOF > ~/$phone_ip.ip
+EOF
+echo $$phone_ip >> ~/$phone_ip.ip
+# Nastavi SSH
+echo "■■■■ update to blb ssh ■■■■"
+echo -e "\n\e[93mpreveri IP !!\e[0m\n"
+ssh $my_name@192.168.100.$phone_ip -p 8022
