91d0a6aa4f8853a0074bf825af121885282283b2
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-10-05 16:24:09 +0200

    Update mycpu.sh

--- 0/mycpu.sh
+++ 0/mycpu.sh
@@ -8,6 +8,13 @@ IFS=$'\n' read -rd '' -a cpus <<< "$output"
 MYCPU1="${cpus[0]}"
 MYCPU2="${cpus[1]}"
 
+echo "MYCPU1: $MYCPU1"
+echo "MYCPU2: $MYCPU2"
+
+# V male črke
+MYCPU1=$(echo "$MYCPU1" | tr '[:upper:]' '[:lower:]')
+MYCPU2=$(echo "$MYCPU2" | tr '[:upper:]' '[:lower:]')
+
 # Izpiši vrednosti
 echo "MYCPU1: $MYCPU1"
 echo "MYCPU2: $MYCPU2"
