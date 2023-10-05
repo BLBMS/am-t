93560bdefbd7d4ee81d69cbf51fe73cac202c49f
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-10-05 19:21:53 +0200

    Update configure-moj.sh

--- 0/configure-moj.sh
+++ 0/configure-moj.sh
@@ -38,16 +38,4 @@ if [ "$yn" != "y" ] && [ "$yn" != "Y" ]; then
     exit
 fi
 
-
-exit 
-echo "start compile ..."
-
-ARCH="-march=armv8.3-a+crypto"
-
-#tega sedaj ne rabim
-#CORE="-mtune=cortex-a76 -mtune=cortex-a55"
-
-OPTI="-Ofast -pthread -flto -fstrict-aliasing -ftree-vectorize -funroll-loops -ffinite-loops -finline-functions -fno-stack-protector -fomit-frame-pointer -fpic -falign-functions=64 -D_REENTRANT -mllvm -enable-loop-distribute"
-
-./configure CXXFLAGS="$ARCH $CORE $OPTI" CFLAGS="$ARCH $CORE $OPTI" \
-CXX=clang++ CC=clang  LDFLAGS="-Wl,-O3 -Wl,-hugetlbfs-align -fuse-ld=lld"
+sed -i "s/CCCCCCCCCC/$CORE/g" ~/ccminer/configure.sh
