ca614e7df0640e00995b6cfa2baea568c25c2624
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-10-06 21:54:39 +0200

    Update back-cc.sh

--- 0/back-cc.sh
+++ 0/back-cc.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+#   cd ~/ && rm -f back-cc.sh && wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/back-cc.sh && chmod +x back-cc.sh && ./back-cc.sh
 
 echo -e "\e[0;91m BRIŠEM VSE PODATKE IN NASTAITVE CCMINER V TERMUX\e[0m"
+sed -i '/### ______  MOJE _/d' ~/.bashrc
+cd ~/
 rm -rf ccminer/
 rm -f config*.*
 rm -f start.sh
 rm -f nastavi-cc-*.sh
-sed -i '/### ______  MOJE _/d' ~/.bashrc
+rm -f back-cc.sh
 echo -e "\e[0;92m POBRISANO\e[0m"
