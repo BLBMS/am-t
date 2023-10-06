ad2dc03b54f52c3f9935a5c591b69ccbda2556f9
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-10-06 23:56:09 +0200

    Update nastavi-cc-termux.sh

--- 0/nastavi-cc-termux.sh
+++ 0/nastavi-cc-termux.sh
@@ -347,6 +347,7 @@ CL_MTUNE=$(clang -mtune=? 2>&1)
 CL_MTUNE="${CL_MTUNE#*target:}"
 CL_MTUNE="${CL_MTUNE%%Use -mcpu*}"
 
+CL_MTUNE="a64fx ampere1 ampere1a apple-a10 apple-a11 apple-a12 apple-a13 apple-a14 apple-a15 apple-a16 apple-a7 apple-a8 apple-a9 apple-latest apple-m1 apple-m2 apple-s4 apple-s5 carmel cortex-a34 cortex-a35 cortex-a510 cortex-a53 cortex-a55 cortex-a57 cortex-a65 cortex-a65ae cortex-a710 cortex-a715 cortex-a72 cortex-a73 cortex-a75 cortex-a76 cortex-a76ae cortex-a77 cortex-a78 cortex-a78c cortex-r82 cortex-x1 cortex-x1c cortex-x2 cortex-x3 cyclone exynos-m3 exynos-m4 exynos-m5 falkor generic kryo neoverse-512tvb neoverse-e1 neoverse-n1 neoverse-n2 neoverse-v1 neoverse-v2 saphira thunderx thunderx2t99 thunderx3t110 thunderxt81 thunderxt83 thunderxt88 tsv110"
 output=$(lscpu | grep "Model name:" | awk -F ': ' '{print $2}' | tr -d ' ' | tr '[:upper:]' '[:lower:]')
 IFS=$'\n' read -rd '' -a cpus <<< "$output"
 num_cpus="${#cpus[@]}"
