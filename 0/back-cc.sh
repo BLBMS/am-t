550b0991ffeb58f8231933b9ce484e4ffe3f17b2
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-10-06 21:58:35 +0200

    Update back-cc.sh

--- 0/back-cc.sh
+++ 0/back-cc.sh
@@ -3,7 +3,7 @@
 #   cd ~/ && rm -f back-cc.sh && wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/back-cc.sh && chmod +x back-cc.sh && ./back-cc.sh
 
 echo -e "\e[0;91m BRIŠEM VSE PODATKE IN NASTAITVE CCMINER V TERMUX\e[0m"
-sed -i '/### ______  MOJE _/d' ~/.bashrc
+sed -i '/### ______  MOJE _/,$d' ~/.bashrc
 rm -rf ~/.termux/boot
 cd ~/
 rm -rf ~/ccminer/
