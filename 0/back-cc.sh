6ecdb471e75cfa757bd7a3854b9ff6fc67cbd9f5
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-10-06 21:56:12 +0200

    Update back-cc.sh

--- 0/back-cc.sh
+++ 0/back-cc.sh
@@ -4,10 +4,11 @@
 
 echo -e "\e[0;91m BRIŠEM VSE PODATKE IN NASTAITVE CCMINER V TERMUX\e[0m"
 sed -i '/### ______  MOJE _/d' ~/.bashrc
+rm -rf ~/.termux/boot
 cd ~/
-rm -rf ccminer/
-rm -f config*.*
-rm -f start.sh
-rm -f nastavi-cc-*.sh
-rm -f back-cc.sh
+rm -rf ~/ccminer/
+rm -f ~/config*.*
+rm -f ~/start.sh
+rm -f ~/nastavi-cc-*.sh
+rm -f ~/back-cc.sh
 echo -e "\e[0;92m POBRISANO\e[0m"
