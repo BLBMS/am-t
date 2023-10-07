623bccc594f698a32d9f59a08f71a73f02b79b3c
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-10-07 13:01:45 +0200

    Create copy-S10.sh

---
+++ 0/copy-S10.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#->   cd ~/ && rm -f copy-S10.sh && wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/copy-S10.sh && chmod +x copy-S10.sh && ./copy-S10.sh
+
+# moj dela na S10 (h)
+
+cd ~/ && rm -f ccminer_S10h.tar.gz && wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/ccminer_S10h.tar.gz
