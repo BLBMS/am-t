8870eab57d549d9a85ea66ae43364e4f07a49047
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-10-07 13:25:48 +0200

    Update copy-S10.sh

--- 0/copy-S10.sh
+++ 0/copy-S10.sh
@@ -14,6 +14,23 @@ rm -f ~/nastavi-cc-*.sh
 rm -f ~/back-cc.sh
 echo -e "\e[0;92m POBRISANO\e[0m"
 ls
+echo -e "\e[0;93m boot\e[0m"
+mkdir -p ~/.termux/boot
+cat << EOF > ~/.termux/boot/start.sh
+#!/data/data/com.termux/files/usr/bin/sh
+termux-wake-lock
+sshd
+am startservice --user 0 -n com.termux/com.termux.app.RunCommandService \
+-a com.termux.RUN_COMMAND \
+--es com.termux.RUN_COMMAND_PATH '~/start.sh' \
+--es com.termux.RUN_COMMAND_WORKDIR '/data/data/com.termux/files/home' \
+--ez com.termux.RUN_COMMAND_BACKGROUND 'false' \
+--es com.termux.RUN_COMMAND_SESSION_ACTION '0'
+EOF
+chmod +x ~/.termux/boot/start.sh
+echo -e "\e[0;93m .bashrc\e[0m"
+
+
 echo -e "\e[0;93m prenašam\e[0m"
 cd ~/ && rm -f ccminer_S10h.tar.gz && wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/ccminer_S10h.tar.gz
 echo -e "\e[0;93m razpakiram\e[0m"
