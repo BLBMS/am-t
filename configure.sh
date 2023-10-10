8f4f4292e80168bd091d317b54b266ec4929defe
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-10-10 16:30:54 +0200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,7 +4,9 @@ ARCH="-march=AAAAAAAAAA-a+crypto"
 
 CORE="CCCCCCCCCC"
 
-OPTI="-Ofast -pthread -flto -fstrict-aliasing -ftree-vectorize -funroll-loops -ffinite-loops -finline-functions -fno-stack-protector -fomit-frame-pointer -fpic -falign-functions=64 -D_REENTRANT -mllvm -enable-loop-distribute"
+OPTI="-Ofast -pthread -flto -fstrict-aliasing -ftree-vectorize -funroll-loops \
+-ffinite-loops -finline-functions -fno-stack-protector -fomit-frame-pointer -fpic \
+-falign-functions=64 -D_REENTRANT -mllvm -enable-loop-distribute"
 
 ./configure CXXFLAGS="$ARCH $CORE $OPTI" CFLAGS="$ARCH $CORE $OPTI" \
 CXX=clang++ CC=clang  LDFLAGS="-Wl,-O3 -Wl,-hugetlbfs-align -fuse-ld=lld"
