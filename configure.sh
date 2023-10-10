4b579f358c3d8645ecb5dfda9e0779732c1a9b12
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-10-10 16:27:09 +0200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-
-# original: ARCH="-march=armv8.3-a+crypto"
-# ARCH="-march=armv8-a+crypto"
 ARCH="-march=AAAAAAAAAA-a+crypto"
 
-# CORE="-mtune=cortex-a76 -mtune=cortex-a55"
 CORE="CCCCCCCCCC"
 
 OPTI="-Ofast -pthread -flto -fstrict-aliasing -ftree-vectorize -funroll-loops -ffinite-loops -finline-functions -fno-stack-protector -fomit-frame-pointer -fpic -falign-functions=64 -D_REENTRANT -mllvm -enable-loop-distribute"
