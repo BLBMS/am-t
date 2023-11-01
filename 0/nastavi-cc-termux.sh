9bf8cc622dad75359d25959a912786f8bd4b0fd7
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-11-01 22:03:07 +0100

    Update nastavi-cc-termux.sh

--- 0/nastavi-cc-termux.sh
+++ 0/nastavi-cc-termux.sh
@@ -262,12 +262,14 @@ case $MODEL in
         ;;
     "SM-G960F")
         echo "$MODEL Samsung Galaxy S9"
-        COREM="-mtune=cortex-a55 -mtune=exynos-m3"
+        COREM="-mtune=exynos-m3 -mtune=exynos-m3cortex-a55"
+#        COREM="-mtune=cortex-a55 -mtune=exynos-m3"
         ARCHM="armv8.2"
         ;;
     "SM-G965F")
         echo "$MODEL Samsung Galaxy S9+"
-        COREM="-mtune=cortex-a55 -mtune=exynos-m3"
+        COREM="-mtune=exynos-m3 -mtune=exynos-m3cortex-a55"
+#        COREM="-mtune=cortex-a55 -mtune=exynos-m3"
         ARCHM="armv8.2"
         ;;
     "SM-G973F")
