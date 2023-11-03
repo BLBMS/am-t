c88a3267d54625beea3930b66a45bb664a091547
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-11-03 22:33:29 +0100

    Update start.sh

--- 0/start.sh
+++ 0/start.sh
@@ -4,7 +4,7 @@ if screen -ls | grep -i ccminer;
 then
   printf "\n\e[91m CCminer is already running! \e[0m\n"
 else
-  printf "\n\e[93m Starting CCminer! \e[0m\n"
+  printf "\n\e[92m Starting CCminer! \e[0m\n"
   screen -ls | grep -o "[0-9]\+\." | awk "{print $1}" | xargs -I {} screen -X -S {} quit
   screen -wipe 1>/dev/null 2>&1
   screen -dmS CCminer 1>/dev/null 2>&1
