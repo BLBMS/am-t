9bfaef8ceeec92f09fb1d2d954b75ccc736516dd
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-04 00:50:04 +0100

    Update posodobi.sh

--- 0/posodobi.sh
+++ 0/posodobi.sh
@@ -4,11 +4,17 @@
 
 if [ $# -eq 1 ]; then
     POP=$1
-    echo "Parameter $POP shranjen v spremenljivko POP."
+#    echo "Parameter $POP shranjen v spremenljivko POP."
 elif [ $# -eq 0 ]; then
     read -p "Vnesi številko popravka (01): " POP
-    echo "Parameter $POP shranjen v spremenljivko POP."
+#    echo "Parameter $POP shranjen v spremenljivko POP."
 else
-    echo "Napaka: Pričakovan je en parameter ali noben parameter."
+    echo "Napaka: Pričakovan je en ali noben parameter."
+    exit
 fi
-echo -e "\e[92mPopravek $POP"
+echo -e "\n\e[92mPopravek $POP\e[0m\n"
+
+FAJL="POP$POP"
+cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh;./$FAJL.sh
+
+echo -e "\n\e[92mPopravek $POP končan\e[0m\n"
