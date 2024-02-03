f7f8f6a0d55eedffb52cc2d9d06343ed2d48766c
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-04 00:05:15 +0100

    Update pop01.sh

--- 0/pop01.sh
+++ 0/pop01.sh
@@ -2,20 +2,37 @@
 
 #   POP="01";cd ~/;rm -f pop$POP.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
 
-cd ~/ 
-rm -f inf.sh && wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/inf.sh && chmod +x inf.sh
-
-# Preverimo, ali datoteka .bashrc obstaja
-if [ -f ~/.bashrc ]; then
-    # Preverimo, ali že obstaja zapis za alias
-    if grep -q "alias inf='~/inf.sh'" ~/.bashrc; then
-        echo "Alias 'inf' že obstaja v .bashrc."
-    else
-        # Poiščemo zadnjo vrstico z besedo "alias" in dodamo novo vrstico za njo
-        last_alias_line=$(grep -n "alias" ~/.bashrc | tail -n 1 | cut -d: -f1)
-        sed -i "${last_alias_line}a alias inf='~/inf.sh'" ~/.bashrc
-        echo "Alias 'inf' dodan v .bashrc."
-    fi
+if [ -f ~/*.pool ]; then
+    echo "Pop 01 že izveden."
 else
-    echo "Datoteka .bashrc ne obstaja."
+    cd ~/ 
+    rm -f inf.sh && wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/inf.sh && chmod +x inf.sh
+    
+    # Preverimo, ali datoteka .bashrc obstaja
+    if [ -f ~/.bashrc ]; then
+        # Preverimo, ali že obstaja zapis za alias
+        if grep -q "alias inf='~/inf.sh'" ~/.bashrc; then
+            echo "Alias 'inf' že obstaja v .bashrc."
+        else
+            # Poiščemo zadnjo vrstico z besedo "alias" in dodamo novo vrstico za njo
+            last_alias_line=$(grep -n "alias" ~/.bashrc | tail -n 1 | cut -d: -f1)
+            sed -i "${last_alias_line}a alias inf='~/inf.sh'" ~/.bashrc
+            echo "Alias 'inf' dodan v .bashrc."
+        fi
+    else
+        echo "Datoteka .bashrc ne obstaja."
+    fi
+    
+    cat << EOF >> ~/start.sh
+    
+    POOL=$(sed -n '0,/.*"name": "\(.*\)".*/s//\1/p' config.json | awk '{print $1}')
+    if [ $(echo $POOL | tr -cd '.' | wc -c) -eq 2 ]; then
+        # Obstajata dve pike, vzemimo srednji del
+        POOL=$(echo $POOL | cut -d'.' -f2)
+    fi
+    POOL=$(echo $POOL | tr -d '.')
+    rm -f *.pool
+    echo $POOL > ~/$POOL.pool
+    EOF
+    ./start.sh
 fi
