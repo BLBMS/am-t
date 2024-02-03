15b36fd636563710af21fe7fce2be0228903ac01
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-03 23:19:23 +0100

    Update pop01.sh

--- 0/pop01.sh
+++ 0/pop01.sh
@@ -5,4 +5,17 @@
 cd ~/ 
 rm -f inf.sh && wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/inf.sh && chmod +x inf.sh
 
-
+# Preverimo, ali datoteka .bashrc obstaja
+if [ -f ~/.bashrc ]; then
+    # Preverimo, ali že obstaja zapis za alias
+    if grep -q "alias inf='~/inf.sh'" ~/.bashrc; then
+        echo "Alias 'inf' že obstaja v .bashrc."
+    else
+        # Poiščemo zadnjo vrstico z besedo "alias" in dodamo novo vrstico za njo
+        last_alias_line=$(grep -n "alias" ~/.bashrc | tail -n 1 | cut -d: -f1)
+        sed -i "${last_alias_line}a alias inf='~/inf.sh'" ~/.bashrc
+        echo "Alias 'inf' dodan v .bashrc."
+    fi
+else
+    echo "Datoteka .bashrc ne obstaja."
+fi
