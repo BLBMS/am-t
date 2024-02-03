7fce8544dd4b6eada2d78f6ccbeb0b35a82930ce
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-03 23:11:30 +0100

    Create pop01.sh

---
+++ 0/pop01.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#   POP="01";cd ~/;rm -f pop$POP.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
+
+cd ~/ 
+rm -f inf.sh && wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/inf.sh && chmod +x inf.sh
+
+
