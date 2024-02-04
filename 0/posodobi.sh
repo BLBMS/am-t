00875cfbddee2e33a6b07e9e0187e9930b0b8b4f
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-04 01:01:57 +0100

    Update posodobi.sh

--- 0/posodobi.sh
+++ 0/posodobi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#    FAJL="posodobi";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh;./$FAJL.sh
+#    POP="01";FAJL="posodobi";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh;./$FAJL.sh $POP
 
 if [ $# -eq 1 ]; then
     POP=$1
