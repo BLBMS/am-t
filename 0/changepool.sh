a5dd48acdefbc780dcd8b21e9822543109e42078
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-08 21:18:02 +0100

    Update changepool.sh

--- 0/changepool.sh
+++ 0/changepool.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-HELP="Set pool as attribut\n  1  mrr          MRR\n  2  verus        pool.verus.io\n  3  luck         eu.luckpool.net\n  4  vipor        de.vipor.net\n  5  cloud(iko)   eu.coudiko.io\n  6  zerg         zergpool-solo\n  h  help       this help"
+HELP="Set pool as attribut\n \
+  1  mrr          MRR\n \
+  2  verus        pool.verus.io\n \
+  3  luck         eu.luckpool.net\n \
+  4  vipor        de.vipor.net\n \
+  5  cloud(iko)   eu.coudiko.io\n \
+  6  zerg         zergpool-solo\n \
+  h  help         this help"
+
+#HELP="Set pool as attribut\n  1  mrr          MRR\n  2  verus        pool.verus.io\n  3  luck         eu.luckpool.net\n  4  vipor        de.vipor.net\n  5  cloud(iko)   eu.coudiko.io\n  6  zerg         zergpool-solo\n  h  help       this help"
 #echo -e "$HELP"
 
 if [ $# -eq 1 ]; then
