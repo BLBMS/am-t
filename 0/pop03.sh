fb958b51dd651a905a1a39f8d697ba5df63c441f
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-18 00:52:33 +0100

    Update pop03.sh

--- 0/pop03.sh
+++ 0/pop03.sh
@@ -2,24 +2,19 @@
 
 #   POP="03";cd ~/;rm -f pop$POP.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
 
-if [ -f ~/pop03.sh ]; then
-    echo "Pop 03 že izveden."
-    exit 0
-else
-    cd ~/ 
-    
-    # Preverimo, ali datoteka .bashrc obstaja
-    if [ -f ~/.bashrc ]; then
-        # Preverimo, ali že obstaja zapis za alias
-        if grep -q 'echo -e "\e[94mPool: \e[92m$(basename *.pool .pool)\e[0m"' ~/.bashrc; then
-            echo "pool=xxxx že obstaja v .bashrc."
-        else
-            # Poiščemo zadnjo vrstico z besedo "alias" in dodamo novo vrstico za njo
-            sed -i '/^ss$/,$d' ~/.bashrc
-            echo 'echo -e "\e[94mPool: \e[92m"$(basename *.pool .pool)"\e[0m"'  >> ~/.bashrc
-        fi
+cd ~/ 
+
+# Preverimo, ali datoteka .bashrc obstaja
+if [ -f ~/.bashrc ]; then
+    # Preverimo, ali že obstaja zapis za alias
+    if grep -q 'echo -e "\e[94mPool: \e[92m$(basename *.pool .pool)\e[0m"' ~/.bashrc; then
+        echo "pool=xxxx že obstaja v .bashrc."
     else
-        echo "Datoteka .bashrc ne obstaja."
-        exit
+        # Poiščemo zadnjo vrstico z besedo "alias" in dodamo novo vrstico za njo
+        sed -i '/^ss$/,$d' ~/.bashrc
+        echo 'echo -e "\e[94mPool: \e[92m"$(basename *.pool .pool)"\e[0m"'  >> ~/.bashrc
     fi
+else
+    echo "Datoteka .bashrc ne obstaja."
+    exit
 fi
