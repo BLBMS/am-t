8bcf4f30737749e0106a1751ded319659f2800f6
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-18 01:04:52 +0100

    Update pop03.sh

--- 0/pop03.sh
+++ 0/pop03.sh
@@ -7,12 +7,12 @@ cd ~/
 # Preverimo, ali datoteka .bashrc obstaja
 if [ -f ~/.bashrc ]; then
     # Preverimo, ali že obstaja nov zapis
-    if grep -q 'echo -e "\e\[94mPool: \e\[92m\$(basename \*.pool .pool)\e\[0m"' ~/.bashrc; then
+    if grep -q 'echo -e "\\e\[94mPool: \\e\[92m\\$(basename \\*.pool .pool)\\e\[0m"' ~/.bashrc; then
         echo "pool=xxxx že obstaja v .bashrc."
     else
         # Poiščemo zadnjo vrstico z besedo "ss" in jo pobrišem, ter vse za njo
         sed -i '/^ss$/,$d' ~/.bashrc
-        echo 'echo -e "\e\[94mPool: \e\[92m"$(basename \*.pool .pool)"\e\[0m"'  >> ~/.bashrc
+        echo 'echo -e "\\e\[94mPool: \\e\[92m"$(basename \\*.pool .pool)"\\e\[0m"'  >> ~/.bashrc
     fi
 else
     echo "Datoteka .bashrc ne obstaja."
