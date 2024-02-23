061b49663e889dc667d3ad15d64f2c208ba13cbe
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-23 23:57:12 +0100

    Update pop04.sh

--- 0/pop04.sh
+++ 0/pop04.sh
@@ -8,6 +8,8 @@ mv start.sh start_old.sh
 rm -f start.sh
 mv start2.sh start.sh
 touch auto.1
+FAJL="config_blank.json";cd ~/;rm -f $FAJL;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL
 FAJL="poolupdate";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh
 FAJL="cron";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh
-source ./$FAJL.sh
+source ./start.sh
+source ./cron.sh
