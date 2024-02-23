2c024d1152e53445c797656359573d717e641381
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-23 22:51:48 +0100

    Update and rename pool.set to pool.sh

---
+++ 0/pool.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#    FAJL="pool";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh; # ./$FAJL.sh
+
+#NAME="DE vipor"
+#POOL="de.vipor.net:5040"
+
+NAME="FR vipor"
+POOL="fr.vipor.net:5040"
