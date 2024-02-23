399039e7c50cf7493fd962253ec00af3c649919b
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-23 23:55:36 +0100

    Update poolupdate.sh

--- 0/poolupdate.sh
+++ 0/poolupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#   FAJL="start2";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh;./$FAJL.sh
+#   FAJL="poolupdate";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh;./$FAJL.sh
 
 sshd
 screen -wipe 1>/dev/null 2>&1
