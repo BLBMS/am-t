743458abed2efca0faefc929f4e4fec08bf5fb3f
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-23 23:42:32 +0100

    Update cron.sh

--- 0/cron.sh
+++ 0/cron.sh
@@ -3,4 +3,8 @@
 #   FAJL="cron";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh;#./$FAJL.sh
 
 # Nastavitev cron da preveri vsako uro pool
-(crontab -l ; echo "0 * * * * /pot/do/poolupdate.sh") | crontab -
+if [ -e "auto.1" ]; then
+  (crontab -l ; echo "0 * * * * /pot/do/poolupdate.sh") | crontab -
+else
+  echo
+fi
