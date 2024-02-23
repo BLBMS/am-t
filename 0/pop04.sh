b65da1211fb325c35ab239e8d6c1aec55c217086
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-23 23:51:03 +0100

    Update pop04.sh

--- 0/pop04.sh
+++ 0/pop04.sh
@@ -3,4 +3,7 @@
 #   POP="04";cd ~/;rm -f pop$POP.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
 
 cd ~/ 
-cp start.sh start_old.sh
+FAJL="start2";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh
+mv start.sh start_old.sh
+mv start2.sh start.sh
+touch auto.1
