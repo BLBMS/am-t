c499361e719537e566a965d8d888f65f5e85e20d
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-23 23:49:09 +0100

    Create pop04.sh

---
+++ 0/pop04.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#   POP="04";cd ~/;rm -f pop$POP.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
+
+cd ~/ 
+cp start.sh start_old.sh
