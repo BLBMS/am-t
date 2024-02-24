05c9ba36bdf09464ccef24cdc217e62556f914b9
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-24 03:48:03 +0100

    Update pop05.sh

--- 0/pop05.sh
+++ 0/pop05.sh
@@ -9,3 +9,12 @@ mv start.sh start_nej_ok.sh
 rm -f start.sh
 mv start_old.sh start.sh
 ./start.sh
+
+
+exit
+
+POP="03";cd ~/;rm -f pop$POP.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
+POP="04";cd ~/;rm -f pop$POP.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
+POP="start";cd ~/;rm -f $POP.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$POP.sh;chmod +x $POP.sh
+POP="05";cd ~/;rm -f pop$POP.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
+rr
