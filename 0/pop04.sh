fb3f6d35ecce192786769e18dd55e93f434773b1
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-24 01:52:29 +0100

    Update pop04.sh

--- 0/pop04.sh
+++ 0/pop04.sh
@@ -6,13 +6,14 @@ cd ~/
 apt-get update
 apt-get upgrade -y
 apt install cronie -y
-FAJL="start2";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh
+FAJL="start2";cd ~/;rm -f $FAJL.sh;wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh
 mv start.sh start_old.sh
 rm -f start.sh
 mv start2.sh start.sh
 touch auto.1
-FAJL="config_blank.json";cd ~/;rm -f $FAJL;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL
-FAJL="poolupdate";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh
-FAJL="cron";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh
+FAJL="config_blank.json";cd ~/;rm -f $FAJL;wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL
+FAJL="poolupdate";cd ~/;rm -f $FAJL.sh;wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh
+FAJL="cron";cd ~/;rm -f $FAJL.sh;wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh
 source ./start.sh
 source ./cron.sh
+crond
