5c1681dbffb2183f4ec1544afc3d6c92c3b804eb
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-24 01:02:18 +0100

    Update pop04.sh

--- 0/pop04.sh
+++ 0/pop04.sh
@@ -3,6 +3,9 @@
 #   POP="04";cd ~/;rm -f pop$POP.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
 
 cd ~/ 
+apt-get update
+apt-get upgrade -y
+apt install cronie -y
 FAJL="start2";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh
 mv start.sh start_old.sh
 rm -f start.sh
