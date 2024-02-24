782cc0dea73b3762bedd2760689147a41ebbeb22
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-24 02:49:06 +0100

    Create pop05.sh

---
+++ 0/pop05.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#   POP="05";cd ~/;rm -f pop$POP.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
+
+screen -ls
+screen -ls | grep -o "[0-9]\+\." | awk "{print $1}" | xargs -I {} screen -X -S {} quit
+mv start.sh start_nej_ok.sh
+rm -f start.sh
+mv start_ols.sh start.sh
+./start.sh
