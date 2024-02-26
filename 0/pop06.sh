8e3394dec5dd89329e1f4901640240de1f6135c3
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-26 17:25:20 +0100

    Update pop06.sh

--- 0/pop06.sh
+++ 0/pop06.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#   POP="06";cd ~/;rm -f pop$POP.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
+#   POP="06";cd ~/;rm -f pop$POP.sh;wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
 
 cd ~/
-FAJL="ccupdate";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh
+FAJL="ccupdate";cd ~/;rm -f $FAJL.sh;wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh
 
 # Preverimo, ali datoteka .bashrc obstaja
 if [ -f ~/.bashrc ]; then
