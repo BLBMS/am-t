296bdc13aee18be73383b67e9ccc387e3da69e04
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-26 17:34:49 +0100

    Update start3.sh

--- 0/start3.sh
+++ 0/start3.sh
@@ -28,56 +28,18 @@ if screen -list | grep -q "CCminer" && [ "$NAME" = "$obst_pool" ]; then
   sl
 else
   # zamenja pool
-  
-
-
-
-
-fi
-
-
-
-
-exit
-#---------------------------------------
-
   cd ~/
   rm -f config.json
   cp config_blank.json config.json
   sed -i "s/NAME/$NAME/g; s/POOL/$POOL/g; s/DELAVEC/$delavec/g" config.json
-  printf "\n\e[92m Starting CCminer on \e[95mNEW POOL \e[0m\n"
+  echo -e "\n\e[91m Starting CCminer on NEW POOL \e[0m\n"
   screen -ls | grep -o "[0-9]\+\." | awk "{print $1}" | xargs -I {} screen -X -S {} quit
   screen -wipe 1>/dev/null 2>&1
   screen -dmS CCminer 1>/dev/null 2>&1
   screen -S CCminer -X stuff "~/ccminer -c ~/config.json\n" 1>/dev/null 2>&1
-else
-#  STARI start.sh ->
-  if screen -ls | grep -i ccminer;
-  then
-    printf "\n\e[91m CCminer is already running! \e[0m\n"
-  else
-    printf "\n\e[92m Starting CCminer! \e[0m\n"
-    screen -ls | grep -o "[0-9]\+\." | awk "{print $1}" | xargs -I {} screen -X -S {} quit
-    screen -wipe 1>/dev/null 2>&1
-    screen -dmS CCminer 1>/dev/null 2>&1
-    screen -S CCminer -X stuff "~/ccminer -c ~/config.json\n" 1>/dev/null 2>&1
-  fi
-   #  <- STARI start.sh
+  screen -dmS Update 1>/dev/null 2>&1
+  screen -S Update -X stuff "~/ccupdate.sh\n" 1>/dev/null 2>&1
+  rm -f *.pool
+  echo $NAME > ~/$NAME.pool
+  echo -e "\e[93m New Pool: \e[92m$NAME \e[96m$POOL\e[0m"
 fi
-echo -e "\n\e[93mss = start ccminer"
-echo "xx = kill screen"
-echo "sl = list screen"
-echo "rr = show screen"
-echo "hh = this help"
-echo -e "exit: CTRL-a + d\e[0m"
-echo "__________________"
-screen -ls | grep --color=always "CCminer"
-POOL=$(sed -n '0,/.*"name": "\(.*\)".*/s//\1/p' config.json | awk '{print $1}')
-if [ $(echo $POOL | tr -cd '.' | wc -c) -eq 2 ]; then
-    POOL=$(echo $POOL | cut -d'.' -f2)
-fi
-POOL=$(echo $POOL | tr -d '.')
-rm -f *.pool
-echo $POOL > ~/$POOL.pool
-echo -e "\e[94mPool: \e[92m$POOL\e[0m"
-crond
