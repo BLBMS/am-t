25dfa1ef4c400c95c2aaf32bc814de7113c35e5d
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-26 16:25:38 +0100

    Update pop6.sh

--- 0/pop6.sh
+++ 0/pop6.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+#   POP="06";cd ~/;rm -f pop$POP.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
+
+cd ~/
+
+
 v bashrc zamenjaj rr = screen -r CCminer
 dodaj  ru = screen -r Update
