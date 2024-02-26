bac8a9db08db6b00285eac4df2a7fc8e220f4cd4
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-26 16:27:03 +0100

    Update and rename pop6.sh to pop06.sh

--- 0/pop06.sh
+++ 0/pop06.sh
@@ -3,7 +3,7 @@
 #   POP="06";cd ~/;rm -f pop$POP.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
 
 cd ~/
-
+FAJL="ccupdate";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh
 
 v bashrc zamenjaj rr = screen -r CCminer
 dodaj  ru = screen -r Update
