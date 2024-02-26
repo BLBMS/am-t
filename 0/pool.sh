b03391f98a3dcacf4e27b4995100856f947eec5f
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-26 19:33:36 +0100

    Update pool.sh

--- 0/pool.sh
+++ 0/pool.sh
@@ -2,8 +2,8 @@
 
 #    FAJL="pool";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh; # ./$FAJL.sh
 
-#NAME="DE_vipor";POOL="de.vipor.net:5040"
-NAME="DE_vipor_SOLO";POOL="de.vipor.net:5045"
+NAME="DE_vipor";POOL="de.vipor.net:5040"
+#NAME="DE_vipor_SOLO";POOL="de.vipor.net:5045"
 #NAME="FR_vipor";POOL="fr.vipor.net:5040"
 #NAME="FI_vipor";POOL="fi.vipor.net:5040"
 #NAME="PL_vipor";POOL="pl.vipor.net:5040"
