3bdf99630bf46aff88401d7fd66adad5677a7ab1
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-02-26 17:41:54 +0100

    Update ccupdate.sh

--- 0/ccupdate.sh
+++ 0/ccupdate.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 #  vsako polno uro preveri če je nastavljen nov pool
+while true; do
+    # trenutni čas
+    current_time=$(date +%M)
 
+    # Preverite, ali je trenutna minuta 00 (polna ura)
+    if [[ $current_time == "00" ]]; then
+        # Tukaj vstavite ukaze, ki jih želite izvesti ob polni uri
+        #NEKAJ
+    fi
 
-
-
-
-sleep 3540 # počaka 59 minut
+    # Počakajte 1 minuto, preden preverite znova
+    sleep 3480 # počaka 58 minut
+done
