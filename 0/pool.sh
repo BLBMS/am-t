258d324ffff7939106ca0a1e7614b443d2c04e48
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-03-12 17:18:08 +0100

    Update pool.sh

--- 0/pool.sh
+++ 0/pool.sh
@@ -3,7 +3,7 @@
 #    FAJL="pool";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh; # ./$FAJL.sh
 
 #NAME="DE_vipor";POOL="de.vipor.net:5040";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC":PASS=""
-NAME="DE_vipor_SOLO";POOL="de.vipor.net:5045"
+#NAME="DE_vipor_SOLO";POOL="de.vipor.net:5045"
 #NAME="DE_vipor_SOLO";POOL="de.vipor.net:5045";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC":PASS=""
 #NAME="FR_vipor";POOL="fr.vipor.net:5040";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC":PASS=""
 #NAME="FI_vipor";POOL="fi.vipor.net:5040";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC":PASS=""
@@ -11,4 +11,4 @@ NAME="DE_vipor_SOLO";POOL="de.vipor.net:5045"
 #NAME="verus";POOL="pool.verus.io:9998";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC":PASS=""
 #NAME="luckpool";POOL="eu.luckpool.net:3956";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC":PASS=""
 #NAME="cloudiko";POOL="eu.cloudiko.io:9999";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC":PASS=""
-#NAME="MRR";POOL="stratum+tcp://eu-de01.miningrigrentals.com:54157";USER="BLB.295007":PASS="DELAVEC"
+NAME="MRR";POOL="stratum+tcp://eu-de01.miningrigrentals.com:54157";USER="BLB.295007":PASS="DELAVEC"
