d49cc64da7236f081ceaf13515b05331c9ee1b17
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-04-24 18:28:04 +0200

    Update currhash.sh

--- 0/currhash.sh
+++ 0/currhash.sh
@@ -3,6 +3,10 @@ delavec="$(basename ~/*.ww .ww)"
 # URL naslov
 url="https://luckpool.net/verus/worker/RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.$delavec"
 
+# blocks https://luckpool.net/verus/blocks/RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc
+# miner  https://luckpool.net/verus/miner/RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc
+# worker https://luckpool.net/verus/worker/RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.worker
+
 # Pridobitev podatkov iz URL-ja in iskanje vrednosti za "hashrateString"
 podatki=$(curl -s $url)
 curhash=$(echo "$podatki" | grep -o '"hashrateString":"[^"]*' | cut -d'"' -f4)
