dc99f4a0dff8da7f72d8512bcced526df82342f6
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-04-24 18:02:32 +0200

    Update currhash.sh

--- 0/currhash.sh
+++ 0/currhash.sh
@@ -8,4 +8,4 @@ podatki=$(curl -s $url)
 curhash=$(echo "$podatki" | grep -o '"hashrateString":"[^"]*' | cut -d'"' -f4)
 
 # Izpis rezultata
-echo -e "Current hash (luckpool) \n $delavec: $curhash"
+echo -e "Current hash \e[0;94m(luckpool) \n \e[0;93m$delavec\e[0m: \e[0;92m$curhash\e[0m"
