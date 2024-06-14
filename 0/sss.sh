8b2111a5aba270120dafac4da452df0ccc088077
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-06-15 00:57:30 +0200

    Create sss.sh

---
+++ 0/sss.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# v.2024.06.14
+#
+#  ssh z uporabo devices.json
+#->  
+
+check_IP3() {
+  while true; do
+    read -r -n 1 -p "range: 0 1 2: " range
+    echo
+    if [[ "$range" =~ ^[012]$ ]]; then
+      break
+    else
+      echo "Neveljaven vnos! Vnesite 0, 1 ali 2."
+    fi
+  done
+}
+check_IP4() {
+  while true; do
+    read -r -p "IP or worker: " check_IP4
+    echo
+    if [[ "$check_IP4" =~ ^[0-9]{1,3}$ && "$check_IP4" -ge 0 && "$check_IP4" -le 254 ]]; then
+      break
+    elif [[ "$check_IP4" =~ ^[a-zA-Z0-9]+$ ]]; then
+      break
+    else
+      echo "Neveljaven vnos! Vnesite številko med 0 in 254 ali ime delavca (brez presledka)."
+    fi
+  done
+}
+
+if [ $# -eq 2 ]; then
+    range=$1
+    choice=$2
+  else
+    if [ $# -eq 1 ]; then
+      range=0
+      choice=$1
+    else
+      check_IP3
+      check_IP4
+    fi
+fi
+if [[ $choice =~ ^[0-9]+$ ]]; then
+    ip3="10$range"
+    ip4=$choice
+    ip="192.168.$IP3.$IP4"
+    echo "1 ip="$ip
+    exit
+    
+    device=$(grep -w "$ip" spisek.list | cut -f 2)
+    device=$(echo "$device" | sed "s/$ip//g")
+    device=$(echo "$device" | sed 's/   *//g')
+#    echo "1 ip="$ip
+#    echo "1 de="$device
+else
+    device=$choice
+    ip=$(grep -F "$choice" spisek.list | awk -F'\t' '{print $1}')
+    ip=$(echo "$ip" | sed "s/$device//g")
+    ip=$(echo "$ip" | sed 's/   *//g')
+#    echo "2 ip="$ip
+#    echo "2 de="$device
+fi
+if [[ -z "$ip" ]] || [[ -z "$device" ]]; then
+    echo "Naprave $device$ip ni v 'spisek.list'."
+    exit 1
+else
+    echo "Zagon SSH za napravo: $device / 192.168.100.$ip"
+
+    ssh -t -i ~/.ssh/id_blb blb@192.168.100.$ip -p 8022
+
+# "bash -ic './start-ubuntu.sh'"
+
+# za putty    putty -ssh -i /.ssh/id_pu.ppk blb@192.168.100.$ip -P 8022 -geometry 120x60 -bg "#390060" -title "[ $device @ $ip ]" -fn "Monospace 16"
+
+    echo "  exit SSH"
+fi
