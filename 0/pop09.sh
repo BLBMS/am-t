a3f44260194002c5223bcb05441dacc67ca1ae59
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-07-06 15:35:50 +0200

    Update pop09.sh

--- 0/pop09.sh
+++ 0/pop09.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+
+#   POP="09";cd ~/;rm -f pop$POP.sh;wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
+
+FAJL="load.sh";cd ~/;rm -f $FAJL;wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL;chmod +x $FAJL
 
