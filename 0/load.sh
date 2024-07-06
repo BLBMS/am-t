f8807114fedf893e57081005f26583ecf4713458
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-07-06 15:54:00 +0200

    Update load.sh

--- 0/load.sh
+++ 0/load.sh
@@ -10,6 +10,8 @@ if ! [ -z "$1" ]; then
   if [[ "${1: -3}" == ".sh" ]]; then
     chmod +x $1
   fi
+fi
+if ls "$1" > /dev/null 2>&1; then
   echo -e "\e[0;93mFile: $1 loaded.\e[0m"
 else
   echo -e "\e[0;92mFile: $1 NOT loaded.\e[0m"
