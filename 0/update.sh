f202dbbd629cf7035e3e80a2afbf33835141c8ae
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-07-17 00:38:35 +0200

    Update update.sh

--- 0/update.sh
+++ 0/update.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 # v.2024-07-16
-# Osnovni imenik
-BASE_DIR="."
+# Osnovni imenik je trenutni imenik
 rm -f update.list
 wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/update.list
 
 # Pot do datoteke update.list
-UPDATE_LIST="$BASE_DIR/update.list"
+UPDATE_LIST="update.list"
 
 # Preberi update.list in preveri vsako datoteko
 while IFS=' ' read -r file new_date; do
-    if [ -f "$BASE_DIR/$file" ]; then
+    if [ -f "$file" ]; then
         # Preberi trenutni datum iz datoteke
-        current_date=$(grep -oP '(?<=# v\.)\d{4}-\d{2}-\d{2}' "$BASE_DIR/$file")
+        current_date=$(grep -oP '(?<=# v\.)\d{4}-\d{2}-\d{2}' "$file")
 
         # Primerjaj datuma
         if [[ "$new_date" > "$current_date" ]]; then
             echo "Updating $file..."
             # Dodaj URL za prenos datoteke
             URL="https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$file"
-            rm -f "$BASE_DIR/$file"
-            wget -P "$BASE_DIR" "$URL"
+            rm -f "$file"
+            wget "$URL"
         else
             echo "$file is up to date."
         fi
@@ -28,6 +27,6 @@ while IFS=' ' read -r file new_date; do
         echo "$file does not exist. Downloading..."
         # Dodaj URL za prenos datoteke
         URL="https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$file"
-        wget -P "$BASE_DIR" "$URL"
+        wget "$URL"
     fi
 done < "$UPDATE_LIST"
