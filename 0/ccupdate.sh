3823f1d2b3613ff9ad1025eb34c38dd9bb4794a3
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-07-17 18:30:49 +0200

    Update ccupdate.sh

--- 0/ccupdate.sh
+++ 0/ccupdate.sh
@@ -60,8 +60,8 @@ while true; do
       echo
     fi
     # vsak dan po 22:00 preveri posodobitve
-    #if [[ "$(date +%H)" == "22" ]]; then
-    if [[ "$(date +%H:%M)" == "18:30" ]]; then # test
+    if [[ "$(date +%H)" == "19" ]]; then
+    #if [[ "$(date +%H:%M)" == "18:30" ]]; then # test
       if ! [ -f "update.sh" ]; then
         FAJL="update.sh"
         rm -f $FAJL
