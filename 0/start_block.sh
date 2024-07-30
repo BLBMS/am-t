e9a32a611ff9b622b7867fab5c77d45d8ac9b05f
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-07-30 10:18:46 +0200

    Update start_block.sh

--- 0/start_block.sh
+++ 0/start_block.sh
@@ -3,21 +3,22 @@
 #   FAJL="start_block";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh;./$FAJL.sh
 screen -wipe 1>/dev/null 2>&1
 cd ~/
-FAJL="blockupdate.sh"
+FAJL="block_update.sh"
 rm -f $FAJL
 wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL
 chmod +x $FAJL
-if screen -list | grep -q "blockupdate"; then
+if screen -list | grep -q "block_update"; then
   # že deluje
-  echo -e "\e[93m  blockupdate allready started\e[0m"
+  echo -e "\e[93m  block_update allready started\e[0m"
+  exit
 else
   # zažene
   cd ~/
-  echo -e "\n\e[0;92m Starting blockupdate (luckpool) \e[0m\n"
+  echo -e "\n\e[0;92m Starting block_update (luckpool) \e[0m\n"
   #screen -ls | grep -o "[0-9]\+\." | awk "{print }" | xargs -I {} screen -X -S {} quit
   screen -ls
   screen -wipe 1>/dev/null 2>&1
-  screen -dmS blockupdate 1>/dev/null 2>&1
-  screen -S blockupdate -X stuff "~/blockupdate.sh\n" 1>/dev/null 2>&1
-  screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g; s/blockupdate/\x1b[33m&\x1b[0m/g" | tail -n +2 | head -n -1
+  screen -dmS block_update 1>/dev/null 2>&1
+  screen -S block_update -X stuff "~/block_update.sh\n" 1>/dev/null 2>&1
+  screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g; s/block_update/\x1b[33m&\x1b[0m/g" | tail -n +2 | head -n -1
 fi
