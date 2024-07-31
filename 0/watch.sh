cc00c2b23a0b75adbf40929421744c179b1a6967
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-07-31 17:28:14 +0200

    Update watch.sh

--- 0/watch.sh
+++ 0/watch.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # v.2024-07-31
 #
-#->   cd ~/; rm -f ./watch.sh; wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/watch.sh; chmod +x watch.sh
-#
 # allows access for this IP range. Adjust to your own situation.
 # "api-allow": "192.168.xxx.0/24",
 # enables the API by making it listen on the specified IP address and port. 0.0.0.0 signifies all adapters and IPs.
@@ -12,8 +10,9 @@ spisek="dev.list"
 cd ~/
 rm -f $spisek
 wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$spisek
+i=1
+echo -e "Not listed:\n"
 while read -r line; do
-    echo -e "Not listed:\n"
     # Če se vrstica začne z '#' jo izpiše
     if [[ $line =~ ^# ]]; then
         first_field=$(echo "${line:1}" | awk '{print $1}')
@@ -21,30 +20,31 @@ while read -r line; do
         length=${#first_field}
         spaces=$((15 - length))
         space_string=$(printf '%*s' "$spaces")
-        echo -e "\e[0m$first_field$space_string\e[93m   $second_field \e[0m"
+
+        lengthi=${#i}
+        spacesi=$((3 - lengthi))
+        space_stringi=$(printf '%*s' "$spacesi")
+
+        echo -e "\e[0m$i$space_stringi   $first_field$space_string\e[93m   $second_field \e[0m"
+        ((i++))
     fi
 done < $spisek
-sleep 
-echo "0" > iteration.txt
-if ! [ -f ~/watch-screen.sh ]; then
-    rm -f ./watch-screen.sh
-    wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/watch-screen.sh
-fi
-if ! [ -f ~/spisek.list ]; then
-    rm -f ./spisek.list
-    wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/spisek.list
-fi
-if screen -ls | grep -i watch;
-then
+refreshing=300   # refreshing time in sec
+rm -f iteration.txt
+echo "0" >> iteration.txt
+screen -wipe 1>/dev/null 2>&1
+if screen -ls | grep -i Watch; then
   printf "\n\e[93m WATCH is already running! (rwa) to see \e[0m\n"
+  sleep 2
   screen -r Watch
 else
   printf "\n\e[93m Starting WATCH! \e[0m\n"
   screen -S Watch -X quit 1>/dev/null 2>&1
   screen -wipe 1>/dev/null 2>&1
   screen -dmS Watch 1>/dev/null 2>&1
-  screen -S Watch -X stuff "clear; echo checking phones ...\n\n" 1>/dev/null 2>&1
-  screen -S Watch -X stuff "watch -c -t -n 60 './watch-screen.sh'\n" 1>/dev/null 2>&1
+  screen -S Watch -X stuff "clear \n" 1>/dev/null 2>&1
+  screen -S Watch -X stuff "watch -c -n $refreshing './watch-screen.sh'\n" 1>/dev/null 2>&1
+                                    #  -t
 fi
 printf "\n\e[93m opening screen WATCH \e[0m\n"
 printf "\n\e[93m opening with: screen -r Watch or rwa \e[0m\n"
