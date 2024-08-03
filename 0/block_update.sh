b8be9b2303ffaf2c585616318e49689ce234cbe4
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-08-03 23:11:35 +0200

    Update block_update.sh

--- 0/block_update.sh
+++ 0/block_update.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-# v.2024-07-30
-#  enkrat na dan preveri če so najdeni novi bloki
+# v.2024-08-03
+#  data capture frequency in hours (24 is 1x /day)
+freq=6
+# the first data capture is immediate, then according to frequency from midnight onwards
 iter=1
-time_izvajanja="$(date +%H%M)"
+execution_time="$(date +%H%M)"
 # Pretvori trenutni čas v sekunde do polnoči
-curr_min=$((10#${time_izvajanja:0:2} * 60 + 10#${time_izvajanja:2:2}))
+curr_min=$((10#${execution_time:0:2} * 60 + 10#${execution_time:2:2}))
 min_day=1440
 min_to_midnight=$((min_day - curr_min))
 sleep_time=$((min_to_midnight * 60 - 50))
 while true; do
   echo -n -e "\e[96m== $(date) == ($iter)         \r"
   # Preverite, ali je trenutna minuta 00 (polna ura)  # sekunda +%S minuta +%M ura +%H)
-  if [[ "$(date +%H%M)" -le "$time_izvajanja" ]]; then
+  if [[ "$(date +%H%M)" -le "$execution_time" ]]; then
     # se izvesde ob polnoči in prvem ob zagonu
     echo -n -e "\r\e[96m== $(date) == ($iter)         \r"
     cd ~/
@@ -22,7 +24,7 @@ while true; do
     source ./$FAJL
     iter=$((iter + 1))
     sleep "$sleep_time"
-    time_izvajanja="0003"
+    execution_time="0003"
     sleep_time="80040" # počaka 23 ur 58 minut (23*58*60)
   fi
   sleep 25 # počaka 25 sekund
