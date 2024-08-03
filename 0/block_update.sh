32cb879f4b38fe3cafa6e5a021beb26c622f949c
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-08-03 23:51:05 +0200

    Update block_update.sh

--- 0/block_update.sh
+++ 0/block_update.sh
@@ -2,6 +2,57 @@
 # v.2024-08-03
 #  data capture frequency in hours (24 is 1x /day)
 freq=6    # 24 12 8 6 4...
+
+# Pretvori frekvenco v minute
+freq_minutes=$((freq * 60))
+
+# Izvede prvi zajem podatkov takoj
+iter=1
+execution_time="$(date +%H%M)"
+curr_min=$((10#${execution_time:0:2} * 60 + 10#${execution_time:2:2}))
+min_day=1440
+min_to_midnight=$((min_day - curr_min))
+sleep_time=$((min_to_midnight * 60 - 50))
+
+# Prvi zajem podatkov
+echo -n -e "\e[96m== $(date) == ($iter)         \r"
+cd ~/
+FILE="block_found.sh"
+rm -f $FILE
+wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FILE
+chmod +x $FILE
+source ./$FILE
+iter=$((iter + 1))
+
+# Počakaj do naslednjega intervala
+sleep "$sleep_time"
+
+while true; do
+  current_time="$(date +%H%M)"
+  # Preverite, ali je trenutni čas enak ali večji od časa zajema podatkov
+  if [[ "$current_time" -ge "$execution_time" ]]; then
+    echo -n -e "\r\e[96m== $(date) == ($iter)         \r"
+    cd ~/
+    FILE="block_found.sh"
+    rm -f $FILE
+    wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FILE
+    chmod +x $FILE
+    source ./$FILE
+    iter=$((iter + 1))
+
+    # Nastavi naslednji čas zajema podatkov glede na frekvenco
+    execution_time=$(date -d "today + $freq_minutes minutes" +%H%M)
+    sleep_time=$((freq_minutes * 60))
+  fi
+  sleep 25 # počaka 25 sekund
+done
+
+exit
+__________
+#!/bin/bash
+# v.2024-08-03
+#  data capture frequency in hours (24 is 1x /day)
+freq=6    # 24 12 8 6 4...
 # the first data capture is immediate, then according to frequency from midnight onwards
 iter=1
 execution_time="$(date +%H%M)"
