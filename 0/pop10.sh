945105584180be91f52eea43fdcbc58a7348eaea
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-08-03 22:29:39 +0200

    Update pop10.sh

--- 0/pop10.sh
+++ 0/pop10.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
+#   POP="09";cd ~/;rm -f pop$POP.sh;wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
+
+
+
+
+
 if [[ ! -z "$WINDOW" ]]; then PS1="\[\e[01;31m\][${PS1}\e[01;31m\]]\[\e[0m\]"; fi
