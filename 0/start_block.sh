edf0ed5384282a524287080538668cfac7cbcfd2
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-08-06 19:51:52 +0200

    Update start_block.sh

--- 0/start_block.sh
+++ 0/start_block.sh
@@ -23,13 +23,13 @@ done
 screen -wipe 1>/dev/null 2>&1
 cd ~/
 if screen -list | grep -q "block_update"; then
-  echo -e "\e[93m  block_update already started\e[0m"
-  exit
+    echo -e "\e[93m  block_update already started\e[0m"
+    exit
 else
-  echo -e "\n\e[0;92m Starting block_update (luckpool) \e[0m\n"
-  screen -ls
-  screen -wipe 1>/dev/null 2>&1
-  screen -dmS block_update 1>/dev/null 2>&1
-  screen -S block_update -X stuff "~/block_update.sh\n" 1>/dev/null 2>&1
-  screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g; s/block_update/\x1b[33m&\x1b[0m/g" | tail -n +2 | head -n -1
+    echo -e "\n\e[0;92m Starting block_update (luckpool) \e[0m\n"
+    screen -ls
+    screen -wipe 1>/dev/null 2>&1
+    screen -dmS block_update 1>/dev/null 2>&1
+    screen -S block_update -X stuff "~/block_update.sh\n" 1>/dev/null 2>&1
+    screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g; s/Watch/\x1b[33m&\x1b[0m/g; s/block_update/\x1b[1;35m&\x1b[0m/g" | tail -n +2 | head -n -1
 fi
