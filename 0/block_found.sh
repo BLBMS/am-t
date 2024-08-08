c9734bd3e5ac9c71af50a3d90b7335258cdc0243
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-08-08 22:07:12 +0200

    Update block_found.sh

--- 0/block_found.sh
+++ 0/block_found.sh
@@ -8,6 +8,9 @@ pool_list=$(jq -r '.pool_list[]' block_data.json)
 
 # Funkcija za pridobivanje in obdelavo blokov iz Luckpool
 get_block_luckpool() {
+}
+
+nika() {
     coinl=$(echo "$coin" | tr '[:upper:]' '[:lower:]')
     if [[ "$coinl" == "vrsc" ]]; then
         coinf="verus"
