9a93b761376584ae674f5018b6610af875e80375
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-08-12 13:52:15 +0200

    Update block_found.sh

--- 0/block_found.sh
+++ 0/block_found.sh
@@ -278,7 +278,7 @@ for pool in $active_pools; do
     esac
 
     echo "$coin_list" | while read -r coin; do
-        echo -e "\e[4m\e[1;93mProcessing $coin at $pool pool...\e[0m\e[24m"
+#        echo -e "\e[4m\e[1;93mProcessing $coin at $pool pool...\e[0m\e[24m"
         coinl=$(echo "$coin" | tr '[:upper:]' '[:lower:]')
         if [[ "$coinl" == "vrsc" ]]; then
             coinf="verus"
