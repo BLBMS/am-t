a53d9af9cf61462df13ab1d47d17929aeef2832c
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-08-22 19:36:19 +0200

    Update pool.sh

--- 0/pool.sh
+++ 0/pool.sh
@@ -10,7 +10,7 @@
 #NAME="vipor_LP";POOL="pl.vipor.net:5040";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
 
 # vipor SOLO
-#NAME="vipor_DE_SOLO";POOL="de.vipor.net:5045";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
+NAME="vipor_DE_SOLO";POOL="de.vipor.net:5045";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
 #NAME="vipor_RO_SOLO";POOL="ro.vipor.net:5045";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
 #NAME="vipor_FR_SOLO";POOL="fr.vipor.net:5045";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
 
@@ -21,7 +21,7 @@
 #NAME="farm";POOL="verus.farm:9999";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""    #  merged
 
 # luckpool
-NAME="luckpool";POOL="eu.luckpool.net:3956";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""    #  merged
+#NAME="luckpool";POOL="eu.luckpool.net:3956";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""    #  merged
 
 #cloudiko
 #NAME="cloudiko";POOL="eu.cloudiko.io:9999";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
