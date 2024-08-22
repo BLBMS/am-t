9e552924d0cd80bea786079fb6b7bdd4a366288b
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-08-22 21:17:14 +0200

    Update watch.sh

--- 0/watch.sh
+++ 0/watch.sh
@@ -32,7 +32,7 @@ rm -f iteration.txt
 echo "0" >> iteration.txt
 screen -wipe 1>/dev/null 2>&1
 if screen -ls | grep -i Watch; then
-  printf "\n\e[93m WATCH is already running! (rwa) to see \e[0m\n"
+  printf "\n\e[93m WATCH is already running! (rw) to see \e[0m\n"
   sleep 2
   screen -r Watch
 else
@@ -45,4 +45,4 @@ else
                                     #  -t
 fi
 printf "\n\e[93m opening screen WATCH \e[0m\n"
-printf "\n\e[93m opening with: screen -r Watch or rwa \e[0m\n"
+printf "\n\e[93m opening with: screen -r Watch or rw \e[0m\n"
