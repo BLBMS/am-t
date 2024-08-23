f81d0ba5dc7b4998ea15e25ee3b7c165f7321d35
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-08-23 11:01:26 +0200

    Update watch-screen.sh

--- 0/watch-screen.sh
+++ 0/watch-screen.sh
@@ -40,4 +40,28 @@ C_Off='\x1B[0m'       # izklopi obarvanje
 -e "s/NOT ON LIST/${iRed}&${C_Off}/g" \
 -e "s/OFF LINE/${Red}&${C_Off}/g" \
 -e "s/]/${C_Off}&${C_Off}/g" \
-| column
+ | column
+
+./check-all.sh | jq -c '.[] | [.PHONE,.HOST,.POOL,.MHS]' | sed \
+-e "s/null/${Red}&${C_Off}/g" \
+-e "s/\[\"/${C_Off}&${iYellow}/g" \
+-e "s/\",\"/${C_Off}&${C_Off}/g" \
+-e "s/\"0\./${Red}&${Red}/g" \
+-e "s/\"1\./${iRed}&${iRed}/g" \
+-e "s/\"2\./${iYellow}&${iYellow}/g" \
+-e "s/_/${Gray}&${C_Off}/g" \
+-e "s/mrr/${Green}&${C_Off}/g" \
+-e "s/vipor/${Yellow}&${C_Off}/g" \
+-e "s/luckpool/${Blue}&${C_Off}/g" \
+-e "s/eu.cloudiko.io/${Magenta}&${C_Off}/g" \
+-e "s/pool.verus.io/${Red}&${C_Off}/g" \
+-e "s/zerg-solo/${Green}&${C_Off}/g" \
+-e "s/all/${iRed}&${iRed}/g" \
+-e "s/active/${iGreen}&${iGreen}/g" \
+-e "s/VRSC\/day/${iBlue}&${iBlue}/g" \
+-e "s/USDT\/day/${iWhite}&${iWhite}/g" \
+-e "s/time/${iMagenta}&${iMagenta}/g" \
+-e "s/missing/${iYellow}&${iYellow}/g" \
+-e "s/NOT ON LIST/${iRed}&${C_Off}/g" \
+-e "s/OFF LINE/${Red}&${C_Off}/g" \
+-e "s/]/${C_Off}&${C_Off}/g" > ./check-all.list
