1bb8004c66c247943bc8762bb6b6c1d2fc574e3f
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-08-23 17:14:26 +0200

    Update DL_apoolminer.sh

--- 0/q/DL_apoolminer.sh
+++ 0/q/DL_apoolminer.sh
@@ -8,6 +8,14 @@ mkdir -p ~/apoolminer
 cd ~/apoolminer
 rm -f $FAJL
 wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/q/$FAJL
-
 tar zxf $FAJL
+IME_FAJL="${FAJL%.tar.gz}"
+mv ~/apoolminer/"$IME_FAJL"/* ~/apoolminer/
+> "$IME_FAJL.version"
+rm -rf $IME_FAJL
+
+MY="miner.my"
+rm -f $MY
+wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/q/$FAJL
+
 
