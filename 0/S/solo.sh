6d636a623291e2dca0437b28c07e1783179f6cd6
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-08-31 12:08:19 +0200

    Update solo.sh

--- 0/S/solo.sh
+++ 0/S/solo.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 # v.2024-08-31
 
+#  FAJL="solo";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/S/$FAJL.sh;chmod +x $FAJL.sh;./$FAJL.sh
 
+mv start.sh start.sh.X
 
 FAJL="config-solo.json"
 rm -f $FAJL
-wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL
+wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/S/$FAJL
+
 FAJL="start-solo.sh"
 rm -f $FAJL.sh
-wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh
+wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/S/$FAJL.sh
 chmod +x $FAJL.sh
+
+screen -ls | grep -o "[0-9]\+\." | awk "{print }" | xargs -I {} screen -X -S {} quit
 source ./$FAJL.sh
