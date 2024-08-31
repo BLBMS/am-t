029e7afa33d1e6eab8613649cb112cadb4867e15
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-08-31 13:25:39 +0200

    Update solo.sh

--- 0/S/solo.sh
+++ 0/S/solo.sh
@@ -2,14 +2,15 @@
 # v.2024-08-31
 
 #  FAJL="solo";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/S/$FAJL.sh;chmod +x $FAJL.sh;./$FAJL.sh
-
-mv start.sh start.sh.X
+if [ ! -f "start.sh.X" ]; then
+  mv start.sh start.sh.X
+fi
 
 FAJL="config-solo.json"
 rm -f $FAJL
 wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/S/$FAJL
 
-FAJL="start-solo"
+FAJL="start"
 rm -f $FAJL.sh
 wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/S/$FAJL.sh
 chmod +x $FAJL.sh
