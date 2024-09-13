154ce4e10a9bcd6154fed15e7ea9325110bcf262
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-09-13 13:43:15 +0200

    Update new_ver.sh

--- 0/q/new_ver.sh
+++ 0/q/new_ver.sh
@@ -4,24 +4,38 @@
 # podaj link kot atribut
 # https://github.com/apool-io/apoolminer/releases/download/v2.1.1/apoolminer_linux_autoupdate_v2.1.1.tar.gz
 
-cd "$HOME/apoolminer/"
+if [ "$#" -ne 1 ]; then
+    echo "Podaj link kot argument"
+    exit 1
+fi
 
 file="$1"
-wget $file
-tar -xzf $file
-rm $file
-filedir="${file%.tar.gz}"
-filedirall="$HOME/apoolminer/${file%.tar.gz}"
+regex="^https://github.com/.+"
 
-rm -f apoolminer.old
-mv apoolminer apoolminer.old
-rm -f run.sh.old
-mv run.sh run.sh.old
-rm -f upgrade_and_run.sh.old
-mv upgrade_and_run.sh upgrade_and_run.sh.old
+if [[ "$file" =~ $regex ]]; then
+  
+  cd "$HOME/apoolminer/"
+  
+  wget $file
+  tar -xzf $file
+  rm $file
+  filedir="${file%.tar.gz}"
+  filedirall="$HOME/apoolminer/${file%.tar.gz}"
+  
+  rm -f apoolminer.old
+  mv apoolminer apoolminer.old
+  rm -f run.sh.old
+  mv run.sh run.sh.old
+  rm -f upgrade_and_run.sh.old
+  mv upgrade_and_run.sh upgrade_and_run.sh.old
+  
+  cp "$filedirall/apoolminer" .
+  cp "$filedirall/run.sh" .
+  cp "$filedirall/upgrade_and_run.sh" .
+  
+  echo " done"
 
-cp "$filedirall/apoolminer" .
-cp "$filedirall/run.sh" .
-cp "$filedirall/upgrade_and_run.sh" .
-
-echo " done"
+else
+  echo "Argument ni veljavna GitHub povezava"
+  exit 1
+fi
