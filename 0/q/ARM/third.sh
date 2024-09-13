4046d2f59ecf8ae5fc18cdd0a6ff74708ebf26b3
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-09-14 00:54:01 +0200

    Update third.sh

--- 0/q/ARM/third.sh
+++ 0/q/ARM/third.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 # v.2024-09-14
 
-LOG_FILE="$HOME/qubic.log"
-
-# Monitor the log file and execute third.sh based on its content
-tail -F "$LOG_FILE" | while read LINE; do
-    if echo "$LINE" | grep -q "Idle period"; then
-        if ! pgrep -f "third.sh" > /dev/null; then
-            echo "Starting third.sh"
-            nohup bash "$HOME/third.sh" > "$HOME/third.log" 2>&1 &
-        fi
-    else
-        if pgrep -f "third.sh" > /dev/null; then
-            echo "Stopping third.sh"
-            pkill -f "third.sh"
-        fi
-    fi
-done
+echo "Starting third_miner.sh"
+nohup bash "$HOME/third_miner.sh" > "$HOME/third.log" 2>&1 &
