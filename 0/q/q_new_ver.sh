93a5e5bbaab8859730cd5b41767cd6731228a2da
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-09-13 13:29:44 +0200

    Update q_new_ver.sh

--- 0/q/q_new_ver.sh
+++ 0/q/q_new_ver.sh
@@ -4,11 +4,24 @@
 # podaj link kot atribut
 # https://github.com/apool-io/apoolminer/releases/download/v2.1.1/apoolminer_linux_autoupdate_v2.1.1.tar.gz
 
+cd "$HOME/apoolminer/"
+
 file="$1"
-cd apoolminer/
 wget $file
 tar -xzf $file
 rm $file
 filedir="${file%.tar.gz}"
 filedirall="$HOME/apoolminer/${file%.tar.gz}"
 
+rm -f apoolminer.old
+mv apoolminer apoolminer.old
+rm -f run.sh.old
+mv run.sh run.sh.old
+rm -f upgrade_and_run.sh.old
+mv upgrade_and_run.sh upgrade_and_run.sh.old
+
+cp "$filedirall/apoolminer" .
+cp "$filedirall/run.sh" .
+cp "$filedirall/upgrade_and_run.sh" .
+
+echo " done"
