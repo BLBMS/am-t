9ca75b564d9e3bff40d9686860b7678341e3b49d
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-09-14 01:03:26 +0200

    Update third_miner.sh

--- 0/q/ARM/third_miner.sh
+++ 0/q/ARM/third_miner.sh
@@ -2,20 +2,16 @@
 # v.2024-09-14
 
 LOG_FILE="$HOME/qubic.log"
-INTERVAL=10             # na koliko sekund preverja status idle ali work
-found_status=false      # indikator, da je bilo stanje najdeno
+INTERVAL=10             # Interval za preverjanje stanja (v sekundah)
+found_status=false      # Indikator za iskanje statusa
 last_status=""
 
-# ------------------------------------------------------------
-
-#Funkcija za izpis screen -ls v barvah
+# Funkcija za izpis screen -ls v barvah
 function screen_ls {
     screen -ls | sed -E "s/Third/\x1b[1;34m&\x1b[0m/g; s/Qubic/\x1b[1;31m&\x1b[0m/g; s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g; s/Watch/\x1b[33m&\x1b[0m/g; s/block_update/\x1b[1;35m&\x1b[0m/g" | tail -n +2 | head -n -1
 }
 
-# ------------------------------------------------------------
-
-#Funkcija VERUS start
+# Funkcija za zagon VERUS
 function third_start {
     echo -e "\e[1;92mQUBIC = IDLE, starting VERUS\e[0m"
     screen -X -S CCminer quit
@@ -27,7 +23,7 @@ function third_start {
     echo $(date)
 }
 
-#Funkcija VERUS stop
+# Funkcija za ustavitev VERUS
 function third_stop {
     echo -e "\e[1;91mQUBIC = MINING, closing VERUS\e[0m"
     screen -X -S CCminer quit
@@ -35,19 +31,15 @@ function third_stop {
     echo $(date)
 }
 
-# ------------------------------------------------------------
-
-# Funkcija za preverjanje vrstic in določitev stanja
+# Funkcija za preverjanje statusa v log datoteki
 function find_status {
     while IFS= read -r line; do
-        # Preveri, če vrstica vsebuje "mining idle now"
-        if echo "$line" | grep -q "mining idle now"; then
-#            echo -e "\e[0;93mStanje: \e[1;92midle\e[0m"
+        # Preveri, če vrstica vsebuje "Idle period"
+        if echo "$line" | grep -q "Idle period"; then
             status="idle"
             break
-        # Preveri, če vrstica vsebuje "mining work now"
-        elif echo "$line" | grep -q "mining work now"; then
-#            echo -e "\e[0;93mStanje: \e[1;91mwork\e[0m"
+        # Preveri, če vrstica vsebuje "Iterrate"
+        elif echo "$line" | grep -q "Iterrate"; then
             status="work"
             break
         fi
@@ -61,30 +53,21 @@ echo -e "\e[0;93mInitial status: \e[1;92m$status\e[0m"
 screen_ls
 echo $(date)
 
-# ------------------------------------------------------------
-
 # Zanka za redno preverjanje stanja
 while true; do
     find_status
     current_status=$status
-#   echo -e "\e[0;93mCurrent status: \e[1;92m$status\e[0m"
 
     # Preveri, ali se je status spremenil
     if [[ "$current_status" == "idle" && "$last_status" != "idle" ]]; then
-#    if [[ "$current_status" == "idle" ]]; then
-        if ! screen -ls | grep -q "CCniner"; then
+        if ! screen -ls | grep -q "CCminer"; then
             third_start
             last_status="idle"
-#        else
-#            echo -e "\e[0;93mCCminer je že zagnan\e[0m"
         fi
     elif [[ "$current_status" == "work" && "$last_status" != "work" ]]; then
-#    elif [[ "$current_status" == "work" ]]; then
-        if screen -ls | grep -q "CCniner"; then
+        if screen -ls | grep -q "CCminer"; then
             third_stop
             last_status="work"
-#        else
-#            echo -e "\e[0;93mCCminer je že zaprt\e[0m"
         fi
     fi
     sleep $INTERVAL
