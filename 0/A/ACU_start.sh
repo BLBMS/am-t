341984f225db09efa1cbb28f414c3bb2efc9cb18
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-09-19 10:53:07 +0200

    Update ACU_start.sh

--- 0/A/ACU_start.sh
+++ 0/A/ACU_start.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-# v.2024-09-18
+# v.2024-09-19
 # by blbMS
 
-# Skripta za preverjanje dostopnosti IP naslovov
-
-while true; do
-    echo -e "\e[93m$(date)\e[0m"
-    for IP in {1..8}; do
-        ping -q -c 3 192.168.90.$IP > /dev/null
-
-        if [ $? -ne 0 ]; then
-            echo -e "\e[91m192.168.90.$IP - Destination Host Unreachable\e[0m"
-#        else
-#            echo -e "\e[92m192.168.90.$IP - Reachable\e[0m"
-        fi
-    done
-    echo -e "\e[93m---------------------\e[0m"
-    sleep 300  # Časovni interval med ponovitvami (5 minut)
-done
+screen -wipe 1>/dev/null 2>&1
+cd ~/
+if screen -list | grep -q "ACU"; then
+    echo -e "\e[93m  ACU_ping already running\e[0m\n"
+    screen -ls | sed -E "s/ACU/\x1b[1;36m&\x1b[0m/g" | tail -n +2 | head -n -1
+    exit 0
+else
+    echo -e "\n\e[0;92m  Starting ACU_ping\e[0m\n"
+    screen -wipe 1>/dev/null 2>&1
+    screen -dmS ACU 1>/dev/null 2>&1
+    screen -S ACU -X stuff "$HOME/ACU/ACU_ping.sh\n" 1>/dev/null 2>&1
+    screen -ls | sed -E "s/ACU/\x1b[1;36m&\x1b[0m/g" | tail -n +2 | head -n -1
+fi
