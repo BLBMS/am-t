172694328e4e72d1d210bb7d431474433bc19878
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-10-09 00:12:41 +0200

    Update curr_hash.sh

--- 0/curr_hash.sh
+++ 0/curr_hash.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 # v.2024-07-16
 delavec="$(basename ~/*.ww .ww)"
+pool="$(basename ~/*.pool .pool)"
 # URL naslov
-url="https://luckpool.net/verus/worker/RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.$delavec"
+case $pool in
+    "luckpool")
+        # blocks https://luckpool.net/verus/blocks/RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc
+        # miner  https://luckpool.net/verus/miner/RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc
+        # worker https://luckpool.net/verus/worker/RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.worker
+        url="https://luckpool.net/verus/worker/RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.$delavec"
+        podatki=$(curl -s $url)
+        currhash=$(echo "$podatki" | grep -o '"hashrateString":"[^"]*' | cut -d'"' -f4)
+        currpool=$(jq -r '.pools[0].name' config.json)
+        ;;
+    "vipor")
+        url="https://master.vipor.net/api/pools/verus/miners/RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc/"
+        
+        ;;
+esac
 
-# blocks https://luckpool.net/verus/blocks/RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc
-# miner  https://luckpool.net/verus/miner/RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc
-# worker https://luckpool.net/verus/worker/RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.worker
 
-# Pridobitev podatkov iz URL-ja in iskanje vrednosti za "hashrateString"
-podatki=$(curl -s $url)
-currhash=$(echo "$podatki" | grep -o '"hashrateString":"[^"]*' | cut -d'"' -f4)
 
-# Preberite vrednost ključa "name" iz config.json in jo shranite v spremenljivko CURRPOOL
-currpool=$(jq -r '.pools[0].name' config.json)
 
 # Izpis rezultata
 echo -e "Current: \e[0;94m$currpool\e[0m: \e[0;93m$delavec\e[0m: \e[0;92m$currhash\e[0m"
