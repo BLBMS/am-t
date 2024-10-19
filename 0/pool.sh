4796811c6da2b57e58d0b6e9f31424ae2ba6a7b1
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-10-19 22:25:05 +0200

    Update pool.sh

--- 0/pool.sh
+++ 0/pool.sh
@@ -24,8 +24,8 @@
 #NAME="luckpool";POOL="eu.luckpool.net:3956";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""    #  merged
 
 # MRR
-#NAME="MRR";POOL="eu-de01.miningrigrentals.com:51287";USER="BLB.295007";PASS="DELAVEC"
-NAME="MRR";POOL="eu-de01.miningrigrentals.com:51287";USER="BLB.295007";PASS="MRR"
+NAME="MRR";POOL="eu-de01.miningrigrentals.com:51287";USER="BLB.295007";PASS="DELAVEC"
+#NAME="MRR";POOL="eu-de01.miningrigrentals.com:51287";USER="BLB.295007";PASS="MRR"
 
 # NiceHash     stratum+tcp://verushash.auto.nicehash.com:9200     3GKJ7GBcSkqC5n19zg5WzbYwqqB823zNiJ 
 #NAME="NH";POOL="verushash.auto.nicehash.com:9200";USER="3GKJ7GBcSkqC5n19zg5WzbYwqqB823zNiJ.DELAVEC";PASS="x"
