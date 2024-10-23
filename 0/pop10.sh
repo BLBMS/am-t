f21620a3ad33b84e48437c2e61a73aa099ade271
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-10-23 23:57:54 +0200

    Update pop10.sh

--- 0/pop10.sh
+++ 0/pop10.sh
@@ -7,3 +7,6 @@
 
 
 if [[ ! -z "$WINDOW" ]]; then PS1="\[\e[01;31m\][${PS1}\e[01;31m\]]\[\e[0m\]"; fi
+
+
+alias vipor='delavec=$(basename ~/*.ww .ww);rm ~/vipor.json;wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/vipor.json;sed -i "s#DELAVEC#$delavec#g" ~/vipor.json;screen -X -S Update quit;screen -S CCminer -X stuff "^C";screen -S CCminer -X stuff "~/ccminer -c ~/vipor.json\n"'
