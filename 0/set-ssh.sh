578e462c83bed8974488e8bb3cc949c16bb96a78
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-10-30 21:32:07 +0100

    Update set-ssh.sh

--- 0/set-ssh.sh
+++ 0/set-ssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# v.2024-07-16
+# v.2024-10-20
 #   FAJL="set-ssh";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh;./$FAJL.sh
 # install vse potrebno
 cd ~/
@@ -22,7 +22,12 @@ ssh-keygen -A
 my_name=$(whoami)
 echo "whoami=" $my_name
 #  Ustvari password
-passwd $my_name
+#passwd $my_name
+
+if ! passwd "$my_name" 2>&1 | grep -q "Run passwd without args"; then
+    echo "Napaka 'passwd $my_name'. Zagon brez argumenta"
+    passwd
+fi
 
 # Nastavi IP - za 192.168.100.xxx
 #ifconfig_out=$(ifconfig)
