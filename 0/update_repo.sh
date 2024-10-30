f663454377d3f40d069067dd05d0faa04e805c1c
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-10-30 22:52:35 +0100

    Update update_repo.sh

--- 0/update_repo.sh
+++ 0/update_repo.sh
@@ -4,19 +4,19 @@
 # Funkcija za posodobitev repozitorija in paketov
 update_mirror() {
   MIRROR=$(shuf -n 1 $HOME/termux_eu_mirrors.list)
-  echo "Preizkušam repozitorij: $MIRROR"
+  echo -e "\n\e[96mPreizkušam repozitorij: $MIRROR\e[0m\n"
 
   # Posodobi sources.list z izbranim repozitorijem
   echo "deb $MIRROR stable main" | tee $PREFIX/etc/apt/sources.list
 
   # Poskusi posodobiti pakete
-#  yes | pkg update
-#  yes | pkg upgrade
+  yes | pkg update
+  yes | pkg upgrade
 }
 
 # Poskusi posodobitev, dokler ni uspešna
 while ! update_mirror; do
-  echo "Napaka pri povezovanju z $MIRROR. Poskus z novim repozitorijem..."
+  echo -e "\n\e[91Napaka pri povezovanju z $MIRROR. Poskus z novim...\e[0m\n"
 done
 
-echo "Posodobitev uspešna z repozitorijem: $MIRROR"
+echo -e "\n\e[92Posodobitev uspešna z repozitorijem: $MIRROR\e[0m\n"
