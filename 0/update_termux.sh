fb0bbdd216195196b68b423f5a5d7af20f6708ee
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-10-30 23:16:16 +0100

    Update update_termux.sh

--- 0/update_termux.sh
+++ 0/update_termux.sh
@@ -2,12 +2,28 @@
 # v.2024-10-20
 
 # Funkcija za posodobitev repozitorija
-update_mirror() {
+update_mirror1() {
   MIRROR=$(shuf -n 1 $HOME/termux_eu_mirrors.list)
   echo -e "\n\e[96mRepozitorij: $MIRROR\e[0m\n"
 
   # Posodobi sources.list z izbranim repozitorijem
   echo "deb $MIRROR stable main" | tee $PREFIX/etc/apt/sources.list
+}
+
+update_mirror5() {
+  # Izberi npr. 3 naključne repozitorije iz seznama, da povečaš možnost delujočih
+  MIRRORS=$(shuf -n 5 $HOME/termux_eu_mirrors.list)
+  echo -e "\n\e[96mAdded Repos:\e[93m"
+  echo "$MIRRORS"
+  echo -e "\n\e[0m"
+  
+  # Posodobi sources.list z izbranimi repozitoriji
+  echo "$MIRRORS" | sed 's|^|deb |; s|$| stable main|' | tee $PREFIX/etc/apt/sources.list
+}
+
+update_mirror5() {
+  echo -e "\n\e[96mAdding repos\e[0m"
+  sed 's|^|deb |; s|$| stable main|' $HOME/termux_eu_mirrors.list | tee $PREFIX/etc/apt/sources.list
 }
 
 # Glavna posodobitvena zanka
