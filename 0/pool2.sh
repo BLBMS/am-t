2f70f6cbc75770d162bc5df7e9f6189d846cac91
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-12-10 22:37:49 +0100

    Update pool2.sh

--- 0/pool2.sh
+++ 0/pool2.sh
@@ -3,29 +3,29 @@
 #    FAJL="pool";cd ~/;rm -f $FAJL.sh;wget https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$FAJL.sh;chmod +x $FAJL.sh; # ./$FAJL.sh
 
 # vipor
-NR="1";NAME="vipor_DE";POOL="de.vipor.net:5040";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC":PASS=""
-NR="2";NAME="vipor_FR";POOL="fr.vipor.net:5040";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
-#NR="1";NAME="vipor_RO";POOL="ro.vipor.net:5040";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
-#NR="1";NAME="vipor_FI";POOL="fi.vipor.net:5040";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
-#NR="1";NAME="vipor_LP";POOL="pl.vipor.net:5040";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
+NAME1="vipor_DE";POOL1="de.vipor.net:5040";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC":PASS=""
+NAME2="vipor_FR";POOL2="fr.vipor.net:5040"#;USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
+#NAME="vipor_RO";POOL="ro.vipor.net:5040";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
+#NAME="vipor_FI";POOL="fi.vipor.net:5040";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
+#NAME="vipor_LP";POOL="pl.vipor.net:5040";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
 
 # vipor SOLO
-#NR="1";NAME="vipor_DE_SOLO";POOL="de.vipor.net:5045";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
-#NR="1";NAME="vipor_RO_SOLO";POOL="ro.vipor.net:5045";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
-#NR="1";NAME="vipor_FR_SOLO";POOL="fr.vipor.net:5045";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
+#NAME="vipor_DE_SOLO";POOL="de.vipor.net:5045";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
+#NAME="vipor_RO_SOLO";POOL="ro.vipor.net:5045";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
+#NAME="vipor_FR_SOLO";POOL="fr.vipor.net:5045";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""
 
 # pool.verus
-#NR="1";NAME="verus";POOL="pool.verus.io:9998";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""    #  merged
+#NAME="verus";POOL="pool.verus.io:9998";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""    #  merged
 
 # verus farm
-#NR="1";NAME="farm";POOL="verus.farm:9999";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""    #  merged
+#NAME="farm";POOL="verus.farm:9999";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""    #  merged
 
 # luckpool
-#NR="1";NAME="luckpool";POOL="eu.luckpool.net:3956";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""    #  merged
+#NAME="luckpool";POOL="eu.luckpool.net:3956";USER="RMHY5CQBAMRhtirgwtsxv6GZT512SYs4wc.DELAVEC";PASS=""    #  merged
 
 # MRR
-#NR="1";NAME="MRR";POOL="eu-de01.miningrigrentals.com:51287";USER="BLB.295007";PASS="DELAVEC"
-#NR="1";NAME="MRR";POOL="eu-de01.miningrigrentals.com:51287";USER="BLB.295007";PASS="MRR"
+#NAME="MRR";POOL="eu-de01.miningrigrentals.com:51287";USER="BLB.295007";PASS="DELAVEC"
+#NAME="MRR";POOL="eu-de01.miningrigrentals.com:51287";USER="BLB.295007";PASS="MRR"
 
 # NiceHash     stratum+tcp://verushash.auto.nicehash.com:9200     3GKJ7GBcSkqC5n19zg5WzbYwqqB823zNiJ 
-#NR="1";NAME="NH";POOL="verushash.auto.nicehash.com:9200";USER="3GKJ7GBcSkqC5n19zg5WzbYwqqB823zNiJ.DELAVEC";PASS="x"
+#NAME="NH";POOL="verushash.auto.nicehash.com:9200";USER="3GKJ7GBcSkqC5n19zg5WzbYwqqB823zNiJ.DELAVEC";PASS="x"
