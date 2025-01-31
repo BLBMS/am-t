29d111d4dd60cd2f347d92d5e60093d6faa83ecb
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-02-01 00:41:30 +0100

    Update pop10.sh

--- 0/pop10.sh
+++ 0/pop10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#   POP="09";cd ~/;rm -f pop$POP.sh;wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
+#   POP="10";cd ~/;rm -f pop$POP.sh;wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/pop$POP.sh;chmod +x pop$POP.sh;./pop$POP.sh
 
 
 
@@ -8,5 +8,6 @@
 
 if [[ ! -z "$WINDOW" ]]; then PS1="\[\e[01;31m\][${PS1}\e[01;31m\]]\[\e[0m\]"; fi
 
+alias n='nano'
 
 alias vipor='delavec=$(basename ~/*.ww .ww);rm ~/vipor.json;wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/vipor.json;sed -i "s#DELAVEC#$delavec#g" ~/vipor.json;screen -X -S Update quit;screen -S CCminer -X stuff "^C";screen -S CCminer -X stuff "~/ccminer -c ~/vipor.json\n"'
