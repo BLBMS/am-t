aa37c9ed0978250014b3905fa8a173b021c7a5f5
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-02-05 11:29:59 +0100

    Update ccupdate1.sh

--- 0/ccupdate1.sh
+++ 0/ccupdate1.sh
@@ -46,105 +46,113 @@ while true; do
     if [[ "$(date +%M)" < "02" ]]; then
         # se izvesde ob polni uri
         echo -n -e "\r\e[96m== $(date) == ($iter)         \r"
-        
-        # Podatki iz naprave
-        ip=$(ifconfig 2>/dev/null | grep -oP 'inet \K[\d.]+(?=\s)' | grep -v '127.0.0.1')
-        #echo -e "\e[0m  Device ip  :\e[96m $ip\e[0m"
-        
-        ime_iz_ww=$(basename ~/*.ww)
-        DELAVEC=${ime_iz_ww%.ww}
-        #echo -e "\e[0m  Worker     :\e[96m $DELAVEC\e[0m"
-        
-        ime_iz_pool=$(basename ~/*.pool)
-        obst_pool=${ime_iz_pool%.pool}
-        #echo -e "\e[0m  First pool :\e[96m $obst_pool\e[0m"
-        
-        RAW_POOL=$(api_pc "pool" "$ip" "4068" | tr -d '\0')
-        if [[ "$RAW_POOL" == *"No Connect"* ]]; then
-            API_POOL="\e[91mNo Connect"
+
+        # Preveri če je ccminer sploh aktiven 
+        if ! pgrep -f 'ccminer' >/dev/null; then
+            # če NI začene start.sh
+            source ./start.sh
         else
-            API_POOL=$(echo "$RAW_POOL" | sed -n 's/POOL=\([^;]*\);.*/\1/p')
-        fi
-        #echo -e "\e[0m  Mining pool:\e[96m $API_POOL\e[0m"
-
-        # config file
-        CJOSN="config.json"
-        
-        # Potatki iz github
-        CFAJL="config_orders.json"
-        rm -f $CFAJL
-        wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$CFAJL
-        
-        # Novi podatki za pool v JSON obliki
-        PFAJL="pool.json"
-        rm -f $PFAJL
-        wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$PFAJL
-        
-        # Najdi največjo številko pod "order"
-        MAX_ORDER=$(jq -r '.[].order' "$PFAJL" | sort -n | tail -1)
-        
-        # Preberi podatke za vse order vrednosti od 1 do MAX_ORDER
-        for ((i=1; i<=MAX_ORDER; i++)); do
-            eval NAME$i='$(jq -r ".[] | select(.order==\"'$i'\") | .name" "$PFAJL")'
-            eval POOL$i='$(jq -r ".[] | select(.order==\"'$i'\") | .pool" "$PFAJL")'
-            eval USER$i='$(jq -r ".[] | select(.order==\"'$i'\") | .user" "$PFAJL")'
-            eval PASS$i='$(jq -r ".[] | select(.order==\"'$i'\") | .pass" "$PFAJL")'
-        done
-        
-        # Sestavi podatke od 1 do MAX_ORDER
-        ORDERS=""
-        for ((i=1; i<=MAX_ORDER; i++)); do
-            NAME=$(eval echo \${NAME$i})
-            POOL=$(eval echo \${POOL$i})
-        
-            if [[ -n "$NAME" && -n "$POOL" ]]; then
-                ORDERS+=$(printf '{"name": "%s","url": "stratum+tcp://%s","timeout": 300,"disabled": 0}' "$NAME" "$POOL")
-                # Add comma only if it's not the last entry
-                if [[ $i -ne $MAX_ORDER ]]; then
-                    ORDERS+=","
-                fi
+            # če JE pa preveri če je pravi pool
+            # Podatki iz naprave
+            ip=$(ifconfig 2>/dev/null | grep -oP 'inet \K[\d.]+(?=\s)' | grep -v '127.0.0.1')
+            #echo -e "\e[0m  Device ip  :\e[96m $ip\e[0m"
+            
+            ime_iz_ww=$(basename ~/*.ww)
+            DELAVEC=${ime_iz_ww%.ww}
+            #echo -e "\e[0m  Worker     :\e[96m $DELAVEC\e[0m"
+            
+            #ime_iz_pool=$(basename ~/*.pool)
+            #obst_pool=${ime_iz_pool%.pool}
+            #echo -e "\e[0m  First pool :\e[96m $obst_pool\e[0m"
+            
+            RAW_POOL=$(api_pc "pool" "$ip" "4068" | tr -d '\0')
+            if [[ "$RAW_POOL" == *"No Connect"* ]]; then
+                API_POOL="\e[91mNo Connect"
+            else
+                API_POOL=$(echo "$RAW_POOL" | sed -n 's/POOL=\([^;]*\);.*/\1/p')
             fi
-        done
-        sed -i "s#ORDERS#$ORDERS#g; s#USER#$USER1#g; s#DELAVEC#$DELAVEC#g; s#PASS#$PASS1#g" $CFAJL
-        jq . $CFAJL > $CFAJL.tmp
-        mv  $CFAJL.tmp > $CFAJL
-        #rm -f $CJOSN
-        #jq . $CFAJL > $CJOSN
+            #echo -e "\e[0m  Mining pool:\e[96m $API_POOL\e[0m"
+    
+            # config file
+            CJOSN="config.json"
+            
+            # Potatki iz github
+            CFAJL="config_orders.json"
+            rm -f $CFAJL
+            wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$CFAJL
+            
+            # Novi podatki za pool v JSON obliki
+            PFAJL="pool.json"
+            rm -f $PFAJL
+            wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/$PFAJL
+            
+            # Najdi največjo številko pod "order"
+            #MAX_ORDER=$(jq -r '.[].order' "$PFAJL" | sort -n | tail -1)
+            
+            # Preberi podatke za vse order vrednosti od 1 do MAX_ORDER
+            #for ((i=1; i<=MAX_ORDER; i++)); do
+            #    eval NAME$i='$(jq -r ".[] | select(.order==\"'$i'\") | .name" "$PFAJL")'
+            #    eval POOL$i='$(jq -r ".[] | select(.order==\"'$i'\") | .pool" "$PFAJL")'
+            #    eval USER$i='$(jq -r ".[] | select(.order==\"'$i'\") | .user" "$PFAJL")'
+            #    eval PASS$i='$(jq -r ".[] | select(.order==\"'$i'\") | .pass" "$PFAJL")'
+            #done
+            NAME1='$(jq -r ".[] | select(.order==\"1\") | .name" "$PFAJL")'
+            
+            # Sestavi podatke od 1 do MAX_ORDER
+            #ORDERS=""
+            #for ((i=1; i<=MAX_ORDER; i++)); do
+            #    NAME=$(eval echo \${NAME$i})
+            #    POOL=$(eval echo \${POOL$i})
+            #
+            #    if [[ -n "$NAME" && -n "$POOL" ]]; then
+            #        ORDERS+=$(printf '{"name": "%s","url": "stratum+tcp://%s","timeout": 300,"disabled": 0}' "$NAME" "$POOL")
+            #        # Add comma only if it's not the last entry
+            #        if [[ $i -ne $MAX_ORDER ]]; then
+            #            ORDERS+=","
+            #        fi
+            #    fi
+            #done
+            #sed -i "s#ORDERS#$ORDERS#g; s#USER#$USER1#g; s#DELAVEC#$DELAVEC#g; s#PASS#$PASS1#g" $CFAJL
+            #jq . $CFAJL > $CFAJL.tmp
+            #mv  $CFAJL.tmp > $CFAJL
+            #rm -f $CJOSN
+            #jq . $CFAJL > $CJOSN
 
-# Preverba
-# na rabim (če ccminer ni aktiven): if ! pgrep -f 'ccminer' >/dev/null; then
-#  Če je prvi novi pool enak iz poll-u iz API
-if [ "$NAME1" = "$API_POOL" ]; then
-    # pool je pravi
-    echo -e "\e[93m  Same pool:\e[92m $NAME1 = $API_POOL\e[0m"
-    screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g" | tail -n +2 | head -n -1
-else
-    # zamenja pool
-    echo -e "\e[0;92m Starting CCminer on NEW POOL: $NAME1\e[0m\n"
-    screen -ls | grep -o "[0-9]\+\." | awk "{print }" | xargs -I {} screen -X -S {} quit
-    screen -wipe 1>/dev/null 2>&1
-    sleep 1
-    screen -dmS CCminer 1>/dev/null 2>&1
-    screen -S CCminer -X stuff "~/ccminer -c $CJOSN\n" 1>/dev/null 2>&1
-    screen -dmS Update 1>/dev/null 2>&1
-    screen -S Update -X stuff "~/ccupdate.sh\n" 1>/dev/null 2>&1
-    rm -f *.pool
-    echo "$NAME1" > ~/$NAME1.pool
-    screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g" | tail -n +2 | head -n -1
-fi
+            # Preverba
+            #  Če je prvi novi pool enak iz poll-u iz API
+            if [ "$NAME1" = "$API_POOL" ]; then
+                # pool je pravi
+                echo -e "\e[93m  Same pool:\e[92m $NAME1 = $API_POOL\e[0m"
+                screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g" | tail -n +2 | head -n -1
+            else
+                # zamenja pool
+                echo -e "\e[0;92m Starting CCminer on NEW POOL: $NAME1\e[0m\n"
+                source ./start.sh
+                #screen -ls | grep -o "[0-9]\+\." | awk "{print }" | xargs -I {} screen -X -S {} quit
+                #screen -wipe 1>/dev/null 2>&1
+                #sleep 1
+                #screen -dmS CCminer 1>/dev/null 2>&1
+                #screen -S CCminer -X stuff "~/ccminer -c $CJOSN\n" 1>/dev/null 2>&1
+                #screen -dmS Update 1>/dev/null 2>&1
+                #screen -S Update -X stuff "~/ccupdate.sh\n" 1>/dev/null 2>&1
+                #rm -f *.pool
+                #echo "$NAME1" > ~/$NAME1.pool
+                #screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g" | tail -n +2 | head -n -1
+            fi
+                # Izpis vseh zajetih vrednosti
+                #for ((i=1; i<=MAX_ORDER; i++)); do
+                #        eval "echo -e \"\e[0;93m$i:\e[0;92m \${NAME$i} \e[0;93m/\e[0;94m \${POOL$i} \e[0m\""
+                #done
+                #_________________________________________________________
+        fi
+    fi
 
-# Izpis vseh zajetih vrednosti
-for ((i=1; i<=MAX_ORDER; i++)); do
-        eval "echo -e \"\e[0;93m$i:\e[0;92m \${NAME$i} \e[0;93m/\e[0;94m \${POOL$i} \e[0m\""
-done
-#_________________________________________________________
+    
 
-
-
+    sleep 3480 # počaka 58 minut (58*60)
 
     # Izračunaj sekunde do naslednje polne ure
-    MINUTE=$(date +%M)
-    SEKUNDE_DO_URE=$(( (60 - MINUTE) * 60 ))
-
-    sleep $SEKUNDE_DO_URE
+    #MINUTE=$(date +%M)
+    #SEKUNDE_DO_URE=$(( (60 - MINUTE) * 60 ))
+    #sleep $SEKUNDE_DO_URE
 done
