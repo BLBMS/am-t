cc17f3c870fb414525a2d30fe31cce3ae9cbe3f1
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-02-05 17:24:07 +0100

    Update ccupdate1.sh

--- 0/ccupdate1.sh
+++ 0/ccupdate1.sh
@@ -47,7 +47,7 @@ ccstart() {
     screen -wipe 1>/dev/null 2>&1
     sleep 0.5
     screen -dmS CCminer 1>/dev/null 2>&1
-    screen -S CCminer -X stuff "~/ccminer -c $CJOSN\n" 1>/dev/null 2>&1
+    screen -S CCminer -X stuff "~/ccminer -c ~/config.json\n" 1>/dev/null 2>&1
     screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g" | tail -n +2 | head -n -1
 }
 
