f80408aecb2ff6f5b4c303e9a0301d87c8972627
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-02-05 02:53:17 +0100

    Update start7.sh

--- 0/start7.sh
+++ 0/start7.sh
@@ -45,18 +45,22 @@ api_pc() {
 
 # Podatki iz naprave
 ip=$(ifconfig 2>/dev/null | grep -oP 'inet \K[\d.]+(?=\s)' | grep -v '127.0.0.1')
-echo -e "\e[0m  ip:\e[96m $ip\e[0m"
+echo -e "\e[0m  Device ip  :\e[96m $ip\e[0m"
 
 ime_iz_ww=$(basename ~/*.ww)
 DELAVEC=${ime_iz_ww%.ww}
-echo -e "\e[0m  WORKER:\e[96m $DELAVEC\e[0m"
+echo -e "\e[0m  Worker     :\e[96m $DELAVEC\e[0m"
 
 ime_iz_pool=$(basename ~/*.pool)
 obst_pool=${ime_iz_pool%.pool}
-echo -e "\e[0m  First pool:\e[96m $obst_pool\e[0m"
+echo -e "\e[0m  First pool :\e[96m $obst_pool\e[0m"
 
 RAW_POOL=$(api_pc "pool" "$ip" "4068" | tr -d '\0')
-API_POOL=$(echo "$RAW_POOL" | sed -n 's/POOL=\([^;]*\);.*/\1/p')
+if [[ "$RAW_POOL" == *"No Connect"* ]]; then
+    API_POOL="\e[91mNo Connect"
+else
+    API_POOL=$(echo "$RAW_POOL" | sed -n 's/POOL=\([^;]*\);.*/\1/p')
+fi
 echo -e "\e[0m  Mining pool:\e[96m $API_POOL\e[0m"
 
 # config file
@@ -104,7 +108,7 @@ jq . $CFAJL > $CJOSN
 # Preverba
 # na rabim (če ccminer ni aktiven): if ! pgrep -f 'ccminer' >/dev/null; then
 #  Če je prvi novi pool enak iz poll-u iz API
-if [ "$NAME1" = "$API_POOL" ]; }; then
+if [ "$NAME1" = "$API_POOL" ]; then
     # pool je pravi
     echo -e "\e[93m  Same pool:\e[92m $NAME1 = $API_POOL\e[0m"
     screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g" | tail -n +2 | head -n -1
@@ -120,11 +124,10 @@ else
     screen -S Update -X stuff "~/ccupdate.sh\n" 1>/dev/null 2>&1
     rm -f *.pool
     echo "$NAME1" > ~/$NAME1.pool
-    # Izpis vseh zajetih vrednosti
-    for ((i=1; i<=MAX_ORDER; i++)); do
-      eval echo "Pool $i:"
-      eval echo "$NAME$i"
-      eval echo "$POOL$i"
-    done
     screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g" | tail -n +2 | head -n -1
 fi
+
+# Izpis vseh zajetih vrednosti
+for ((i=1; i<=MAX_ORDER; i++)); do
+        eval "echo -e \"\e[0;93m$i:\e[0;92m \${NAME$i} \e[0;93m/\e[0;94m \${POOL$i} \e[0m\""
+done
