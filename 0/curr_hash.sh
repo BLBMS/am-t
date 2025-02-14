381b98a99e60ea4b3d5434ff187245ef5abe4ebf
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-02-14 01:00:24 +0100

    Update curr_hash.sh

--- 0/curr_hash.sh
+++ 0/curr_hash.sh
@@ -1,6 +1,46 @@
 #!/bin/bash
 # v.2025-02-14
 # by blbMS
+cd ~
+api_pc() {
+    perl -e 'use IO::Socket::INET;
+        my $sock = new IO::Socket::INET(PeerAddr => "'$2'", PeerPort => "'$3'", Proto => "tcp", Timeout => 2) or exit;
+        print $sock "'$1'";
+        print while <$sock>;
+        close($sock);' | tr -d '\0'
+}
+
+ip=$(ifconfig 2>/dev/null | grep -oP 'inet \K[\d.]+(?=\s)' | grep -v '127.0.0.1')
+ime_iz_ww=$(basename ~/*.ww 2>/dev/null)
+DELAVEC=${ime_iz_ww%.ww}
+ime_iz_pool=$(basename ~/*.pool 2>/dev/null)
+obst_pool=${ime_iz_pool%.pool}
+RAW_S=$(api_pc "summary" "$ip" "4068" | tr -d '\0')
+RAW_P=$(api_pc "pool" "$ip" "4068" | tr -d '\0')
+if [[ "$RAW_S" == "No Connection" || "$RAW_P" == "No Connection" ]]; then
+    echo -e "\e[91mNo Connection to miner API.\e[0m"
+else
+    RESPONSE=$(printf "{\"PHONE\":\"$DELAVEC\",\"HOST\":\"$ip\",\""; echo "$RAW_S" | sed -r \
+        's/=/":"/g; s/;/\",\"/g' | sed 's/|/",/g')$(printf "\""; echo "$RAW_P" | sed -r \
+        's/=/":"/g; s/;/\",\"/g' | sed 's/|/"},/g')
+    curr_POOL=$(echo "$RESPONSE" | jq -r '.POOL' 2>/dev/null)
+    curr_KHS=$(echo "$RESPONSE" | jq -r '.KHS' 2>/dev/null)
+    curr_PING=$(echo "$RESPONSE" | jq -r '.PING' 2>/dev/null)
+    [[ -z "$curr_POOL" || "$curr_POOL" == "null" ]] && curr_POOL="N/A"
+    [[ -z "$curr_KHS" || "$curr_KHS" == "null" ]] && curr_KHS="N/A"
+    [[ -z "$curr_PING" || "$curr_PING" == "null" ]] && curr_PING="N/A"
+    echo -e "\e[93mcPOOL:\e[92m $curr_POOL \e[93mcKHS: \e[92m$curr_KHS \e[93mcPING: \e[92m$curr_PING\e[0m"
+fi
+
+
+exit
+_____________
+
+long version
+
+#!/bin/bash
+# v.2025-02-14
+# by blbMS
 
 cd ~
 
