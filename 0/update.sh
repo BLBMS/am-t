6d0ea49ac6cab654fca52f982bcd5a7773c7afaf
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-02-20 21:56:52 +0100

    Update update.sh

--- 0/update.sh
+++ 0/update.sh
@@ -28,14 +28,14 @@ while IFS=' ' read -r file new_date; do
         else
             # Primerjaj datuma
             if [[ "$new_date" > "$current_date" ]]; then
-                #echo -e "\e[0;94mUpdating \e[0;92m$file \e[0;94m- to new version.\e[0m"
+                echo -e "\e[0;94mUpdating \e[0;92m$file \e[0;94m- to new version.\e[0m"
                 rm -f "$file"
                 wget -q "$github/$file"
                 if [[ "$file" == "start.sh" || "$file" == "ccupdate.sh" ]]; then
                     need_restart=1
                 fi
             elif [[ "$new_date" < "$current_date" ]]; then
-                #echo -e "\e[0;91mDOWNGRADING \e[0;93m$file \e[0;91m- to OLD version.\e[0m"
+                echo -e "\e[0;91mDOWNGRADING \e[0;93m$file \e[0;91m- to OLD version.\e[0m"
                 rm -f "$file"
                 wget -q "$github/$file"
                 if [[ "$file" == "start.sh" || "$file" == "ccupdate.sh" ]]; then
@@ -61,6 +61,7 @@ while IFS=' ' read -r file new_date; do
     fi
 done < "$UPDATE_LIST"
 
+exit
 #    programi za brisanje:
 DELETE_LIST="\
 ccminer.*
