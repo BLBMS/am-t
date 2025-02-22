e12cf3b86175d4c564156886740282b2bc27dc30
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-02-22 03:21:30 +0100

    Update start8.sh

--- 0/start8.sh
+++ 0/start8.sh
@@ -24,11 +24,12 @@ start_pool() {
     fi
     sleep 1
     screen -dmS CCminer 1>/dev/null 2>&1
-    screen -S CCminer -X stuff "~/ccminer -c $CJOSN\n" 1>/dev/null 2>&1
+    screen -S CCminer -X stuff "~/ccminer -c ./config.json\n" 1>/dev/null 2>&1
     screen -dmS Update 1>/dev/null 2>&1
     screen -S Update -X stuff "~/ccupdate.sh\n" 1>/dev/null 2>&1
     rm -f *.pool
     echo "$NAME1" > ~/$NAME1.pool
+    sleep 1
     screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g" | tail -n +2 | head -n -1
 }
 
@@ -141,4 +142,4 @@ fi
 for ((i=1; i<=MAX_ORDER; i++)); do
         eval "echo -e \"\e[0;93m$i:\e[0;92m \${NAME$i} \e[0;93m/\e[0;94m \${POOL$i} \e[0m\""
 done
-screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g" | tail -n +2 | head -n -1
+#screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g" | tail -n +2 | head -n -1
