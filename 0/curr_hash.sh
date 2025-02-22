b3b2e0932d4e9bade146cfbe7683bb453db61714
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-02-22 02:40:18 +0100

    Update curr_hash.sh

--- 0/curr_hash.sh
+++ 0/curr_hash.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
 # v.2025-02-22
 # by blbMS
+
+restart() {
+    screen -ls | grep -o "[0-9]\+\." | awk "{print }" | xargs -I {} screen -X -S {} quit
+    if (screen -list | grep -q -i "CCminer"); then
+        killall ccminer
+        screen -ls | grep -o "[0-9]\+\." | awk "{print }" | xargs -I {} screen -X -S {} quit
+        screen -wipe 1>/dev/null 2>&1
+        if (screen -list | grep -q -i "CCminer"); then
+            killall screen
+            screen -ls | grep -o "[0-9]\+\." | awk "{print }" | xargs -I {} screen -X -S {} quit
+            screen -wipe 1>/dev/null 2>&1
+            if (screen -list | grep -q -i "CCminer"); then
+                rm -rf $HOME/.screen/*
+                screen -ls | grep -o "[0-9]\+\." | awk "{print }" | xargs -I {} screen -X -S {} quit
+                screen -wipe 1>/dev/null 2>&1
+            fi
+        fi
+    fi
+    sleep 1
+    screen -dmS CCminer 1>/dev/null 2>&1
+    screen -S CCminer -X stuff "~/ccminer -c $CJOSN\n" 1>/dev/null 2>&1
+    screen -dmS Update 1>/dev/null 2>&1
+    screen -S Update -X stuff "~/ccupdate.sh\n" 1>/dev/null 2>&1
+    rm -f *.pool
+    echo "$NAME1" > ~/$NAME1.pool
+    screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g" | tail -n +2 | head -n -1
+    exit
+}
+
+if ! pgrep -f "ccminer|update.sh" >/dev/null; then
+    restart
+fi
+
 rm -f hardcopy.*
 if (screen -list | grep -q -i "ccminer"); then
     screen -S CCminer -X hardcopy
@@ -23,10 +56,13 @@ if (screen -list | grep -q -i "ccminer"); then
             echo -e "\e[93mcMHS:\e[92m $MHS \e[93mfound before: \e[92m$DIFF_H\e[93m h \e[92m$DIFF_M\e[93m m\e[92m $DIFF_S\e[93m s\e[0m"
         else
             echo -e "\e[93mNo data found!\e[0m"
+            restart
         fi
     else
         echo -e "\e[91mNo WORKING ccminer in CCminer screen!\e[0m"
+        restart
     fi
 else
     echo -e "\e[91mNo CCminer screen!\e[0m"
+    restart
 fi
