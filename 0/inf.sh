6496440dc70a42d4fc7c36feb19a3fbd0fe76ea8
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-04-04 18:40:02 +0200

    Update inf.sh

--- 0/inf.sh
+++ 0/inf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# v.2024-07-16
+# v.2025-04-04
 #   cd ~/ && rm -f inf.sh && wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/inf.sh && chmod +x inf.sh && ./inf.sh
 # Nastavi IP - za 192.168.yyy.zzz
 ifconfig_out=$(ifconfig)
@@ -27,6 +27,7 @@ echo -e "product.cpu.abilist64: \e[0;92m$(getprop ro.product.cpu.abilist64)\e[0m
 echo -e "arm64.variant        : \e[0;92m$(getprop dalvik.vm.isa.arm64.variant)\e[0m"
 echo -e "Android release:     : \e[0;92m$(getprop ro.build.version.release)\e[0m"
 echo -e "build version        : \e[0;92m$(getprop ro.build.version.incremental)\e[0m"
+echo -e "CSC                  : \e[0;92m$(getprop ro.csc.sales_code)\e[0m"
 
 MTUNE=" a64fx ampere1 ampere1a apple-a10 apple-a11 apple-a12 apple-a13 apple-a14 apple-a15 apple-a16 apple-a7 apple-a8 apple-a9 apple-latest apple-m1 apple-m2 \
 apple-s4 apple-s5 carmel cortex-a34 cortex-a35 cortex-a510 cortex-a53 cortex-a55 cortex-a57 cortex-a65 cortex-a65ae cortex-a710 cortex-a715 cortex-a72 cortex-a73 \
