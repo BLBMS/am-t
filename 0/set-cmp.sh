8733824db98dc87fcfdf2fde3e4a8ec8240d8497
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-04-09 00:12:48 +0200

    Update set-cmp.sh

--- 0/set-cmp.sh
+++ 0/set-cmp.sh
@@ -210,7 +210,7 @@ LOCALE=$(getprop ro.product.locale)
 MODEL=$(getprop ro.product.model)
 ANDROID=$(getprop ro.build.version.release)
 echo -e "ROM - locale         : \e[0;93m$ROM - $LOCALE\e[0m"
-echo -e "build - CSC          : \e[0;93m$BUILD_REL/$BUILD_INC - $CSC\e[0m"
+echo -e "android - build - CSC: \e[0;93m$BUILD_REL - $BUILD_INC - $CSC\e[0m"
 echo -e "product.manufacturer : \e[0;93m$(getprop ro.product.manufacturer)\e[0m"
 echo -e "product.model        : \e[0;93m$(getprop ro.product.model)\e[0m"
 echo -e "product.cpu.abilist64: \e[0;93m$(getprop ro.product.cpu.abilist64)\e[0m"
@@ -390,8 +390,35 @@ fi
 cat << EOF >> ~/.bashrc
 ### ______  MOJE _____
 sshd
-PS1='${debian_chroot:+($debian_chroot)}\[\033[0;93m\]$delavec\[\033[0;91m\]@\[\033[0;93m\]$phone_ip\[\033[00m\]:\[\033[01;32m\]\w\[\033[00m\]\$ '
-if [[ ! -z "$WINDOW" ]]; then PS1="\[\e[01;31m\][${PS1}\e[01;31m\]]\[\e[0m\]"; fi
+
+if [[ -z "$(getprop ro.lineage.version)" ]]; then
+
+    echo -e "\033[0;91mSTOCK OS\033[0m"
+
+    PS1='\[\033[0;93m\]S8b\[\033[0;91m\]@\[\033[0;92m\]0.171\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$ '
+    if [[ ! -z "" ]]; then PS1="\[\e[01;31m\][\e[01;31m\]]\[\e[0m\]"; fi
+
+    alias ss='~/start.sh'
+    alias xx='screen -ls | grep -o "[0-9]\+\." | awk "{print }" | xargs -I {} screen -X -S {} quit && screen -ls'
+    alias sl='screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g" | tail -n +2 | head -n -1'
+    alias rr='screen -d -r CCminer'
+    alias ru='screen -d -r Update'
+
+else
+
+    echo -e "\033[0;94mLineage OS\033[0m"
+
+    PS1='\[\033[0;93m\]S8b\[\033[0;94m\]@\[\033[0;92m\]0.171\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$ '
+    if [[ ! -z "" ]]; then PS1="\[\e[01;31m\][\e[01;31m\]]\[\e[0m\]"; fi
+
+    alias ss='tmux new-session -d -s CCminer && tmux send-keys -t CCminer "~/ccminer -c ./config.json" C-m && tmux new-session -d -s Update && tmux send-keys -t CCminer "~/update.sh" C-m'
+    alias xx='tmux kill-session -t CCminer ; tmux kill-session -t Update'
+    alias sl='tmux ls -F "#{session_name}:#{session_id} [#{session_windows} windows] #{session_created}" | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g"'
+    alias rr='tmux attach -t CCminer'
+    alias ru='tmux attach -t Update'
+
+fi
+
 alias n='nano'
 alias ll='ls -alF'
 alias c+='chmod +x '
@@ -399,12 +426,6 @@ alias sob='source ~/.bashrc'
 alias nb='nano ~/.bashrc'
 alias cj='nano ~/config.json'
 alias ccm='~/ccminer -c ~/config.json'
-alias ss='~/start.sh'
-alias xx='screen -ls | grep -o "[0-9]\+\." | awk "{print }" | xargs -I {} screen -X -S {} quit && screen -ls'
-alias sl='screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g" | tail -n +2 | head -n -1'
-alias rr='screen -d -r CCminer'
-alias ru='screen -d -r Update'
-alias oo='OOOOOO'
 alias ch='~/curr_hash.sh'
 alias load='~/load.sh'
 alias XX='xx'
@@ -418,67 +439,30 @@ alias UU='uu'
 alias inf='~/inf.sh'
 alias posodobi='~/posodobi.sh'
 alias uu='yes | pkg update ; yes | pkg upgrade ; pkg install -y wget net-tools nano screen jq'
-alias hh='echo -e "\e[0;93m\
-_________________________________\n\
-ss = start CCminer/Update\n\
-xx = kill all screens\n\
-sl = list screens\n\
-rr = show CCminer\n\
-ru = show Update\n\
-ch = current hash\n\
-oo = current pool\n\
-uu = update/upgrade termux
-inf = show phone info\n\
-hh = this help\n\
-exit: CTRL-a + d\n\
-_________________________________\n\
-posodobi ## = sistem iz github\n\
-pool = posodobi pool.sh iz github\n\
-nb = nano .bashr\n\
-sb = source .bashrc\n\
-_________________________________\e[0m"'
-hh
-echo "Screens:"
-screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g" | tail -n +2 | head -n -1
-# Preveri, ali obstaja katera koli 'Dead' screen seja
-if screen -ls | grep -i 'dead'; then
-  printf "\n\e[91m There are dead screen sessions -> STOP! \e[0m"
-  screen -ls | grep -o "[0-9]\+\.Dead" | awk '{print $1}' | xargs -I {} screen -X -S {} quit
-  screen -wipe 1>/dev/null 2>&1
-  ~/start.sh
-fi
-if ! screen -list | grep -q -i "ccminer" || ! screen -list | grep -q -i "update"; then
-  echo -e "\n\e[0;91m There are no CCminer or no Update\n\e[0m"
-  xx
-  screen -wipe 1>/dev/null 2>&1
-  ~/start.sh
-fi
-bash ./curr_hash.sh
+
+sl
+ch
 EOF
-sed -i 's/OOOOOO/echo -e "\\e[94mPool: \\e[92m$(basename *.pool .pool)\\e[0m"/g' ~/.bashrc
+echo "set tabsize 4" > "$HOME/.nanorc"
 cd ~/
-rm -f ccupdate.sh
-wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/ccupdate.sh
-chmod +x ccupdate.sh
-rm -f update.sh
-wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/update.sh
-chmod +x update.sh
-rm -f load.sh
-wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/load.sh
-chmod +x load.sh
-rm -f changecc.sh
-wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/changecc.sh
-chmod +x changecc.sh
-rm -f inf.sh
-wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/inf.sh
-chmod +x inf.sh
-rm -f curr_hash.sh
-wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/curr_hash.sh
-chmod +x curr_hash.sh
-rm -f start.sh
-wget -q https://raw.githubusercontent.com/BLBMS/am-t/moje/0/start.sh
-chmod +x ~/start.sh
+MYGIT="https://raw.githubusercontent.com/BLBMS/am-t/moje/0"
+F="ccupdate.sh"
+rm -f "$HOME/$F" && wget -O "$HOME/$F" -q "$MYGIT/$F" && chmod +x "$HOME/$F"
+F="update.sh"
+rm -f "$HOME/$F" && wget -O "$HOME/$F" -q "$MYGIT/$F" && chmod +x "$HOME/$F"
+F="load.sh"
+rm -f "$HOME/$F" && wget -O "$HOME/$F" -q "$MYGIT/$F" && chmod +x "$HOME/$F"
+F="changecc.sh"
+rm -f "$HOME/$F" && wget -O "$HOME/$F" -q "$MYGIT/$F" && chmod +x "$HOME/$F"
+F="inf.sh"
+rm -f "$HOME/$F" && wget -O "$HOME/$F" -q "$MYGIT/$F" && chmod +x "$HOME/$F"
+F="curr_hash.sh"
+rm -f "$HOME/$F" && wget -O "$HOME/$F" -q "$MYGIT/$F" && chmod +x "$HOME/$F"
+F="start.sh"
+rm -f "$HOME/$F" && wget -O "$HOME/$F" -q "$MYGIT/$F" && chmod +x "$HOME/$F"
+F="posodobi.sh"
+rm -f "$HOME/$F" && wget -O "$HOME/$F" -q "$MYGIT/$F" && chmod +x "$HOME/$F"
 echo "all done"
-bash ./inf.sh
-echo "$ip_line  $delavec"
+bash "$HOME/inf.sh"
+echo -e "\n\e[93m $ip_line  $delavec\e[0m\n"
 echo -e "\n\e[92m Type EXIT to restart TERMUX\e[0m\n"
