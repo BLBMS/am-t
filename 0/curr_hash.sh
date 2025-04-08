82574eb6b8677cf2f2798d23715e9001bac0e9c5
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-04-08 22:25:48 +0200

    Update curr_hash.sh

--- 0/curr_hash.sh
+++ 0/curr_hash.sh
@@ -35,7 +35,8 @@ if [[ -z "$(getprop ro.lineage.version)" ]]; then
         screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g" | tail -n +2 | head -n -1
         exit
     }
-
+    # --------------------
+    
     if ! pgrep -f "ccminer|update.sh" >/dev/null; then
         restart
     fi
@@ -104,6 +105,7 @@ else
         tmux ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g"
         exit
     }
+    # --------------------
 
     if ! pgrep -f "ccminer|update.sh" >/dev/null; then
         restart
