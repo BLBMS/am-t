c87ce172d53ae5e20f6a1d7499006370f10383bb
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-04-10 18:59:28 +0200

    Update curr_hash.sh

--- 0/curr_hash.sh
+++ 0/curr_hash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# v.2025-04-10.08
+# v.2025-04-10.009
 # loči stock rom / lineage  +  tmux
 cd
 #if [[ -z "$(getprop ro.lineage.version)" ]]; then
@@ -36,29 +36,72 @@ cd
 #else    
     # tmux version
 #    echo "func -> tmux"
-    restart_tmux() {
-        tmux list-sessions | grep -o "^[0-9]\+" | xargs -I {} tmux kill-session -t {}
-        if (tmux list-sessions | grep -q -i "CCminer"); then
-            killall ccminer
-            tmux list-sessions | grep -o "^[0-9]\+" | xargs -I {} tmux kill-session -t {}
-            if (tmux list-sessions | grep -q -i "CCminer"); then
-                killall tmux
-                tmux list-sessions | grep -o "^[0-9]\+" | xargs -I {} tmux kill-session -t {}
+#    restart_tmux() {
+#        tmux list-sessions | grep -o "^[0-9]\+" | xargs -I {} tmux kill-session -t {}
+#        if (tmux list-sessions | grep -q -i "CCminer"); then
+#            killall ccminer
+#            tmux list-sessions | grep -o "^[0-9]\+" | xargs -I {} tmux kill-session -t {}
+#            if (tmux list-sessions | grep -q -i "CCminer"); then
+#                killall tmux
+#                tmux list-sessions | grep -o "^[0-9]\+" | xargs -I {} tmux kill-session -t {}
 #                if (tmux list-sessions | grep -q -i "CCminer"); then
 #                    rm -rf /tmp/tmux-*
 #                    tmux list-sessions | grep -o "^[0-9]\+" | xargs -I {} tmux kill-session -t {}
 #                fi
-            fi
-        fi
-        sleep 1
-        tmux new-session -d -s CCminer "~/ccminer -c ./config.json"
-        tmux new-session -d -s Update "~/ccupdate.sh"
-        rm -f *.pool
-        echo "$NAME1" > ~/$NAME1.pool
-        sleep 1
-        tmux ls -F "#{session_name}:#{session_id} [#{session_windows} windows] #{session_created}" | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g"
-        exit
+#            fi
+#        fi
+#        sleep 1
+#        tmux new-session -d -s CCminer "~/ccminer -c ./config.json"
+#        tmux new-session -d -s Update "~/ccupdate.sh"
+#        rm -f *.pool
+#        echo "$NAME1" > ~/$NAME1.pool
+#        sleep 1
+#        tmux ls -F "#{session_name}:#{session_id} [#{session_windows} windows] #{session_created}" | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g"
+#        exit
+#    }
+
+# Funkcija za združevanje log vnosov
+merge_log_entries() {
+    local input_file="$1"
+    local output_file="$2"
+    
+    # Pripravi začasno datoteko
+    rm -f "$output_file"
+    touch "$output_file"
+    
+    # Združi vrstice, ki pripadajo istemu log vnosu
+    awk '
+    BEGIN { buffer = "" }
+    /^\[[0-9]{4}-[0-9]{2}-[0-9]{2}/ {
+        if (buffer != "") print buffer
+        buffer = $0
+        next
     }
+    { buffer = buffer " " $0 }
+    END { if (buffer != "") print buffer }
+    ' "$input_file" > "$output_file"
+}
+
+# Izboljšana funkcija za ponovni zagon tmux sej
+restart_tmux() {
+    echo -e "\e[93mPonovno zaganjam tmux seje...\e[0m"
+    # Bolj agresivno čiščenje sej
+    tmux list-sessions | awk -F: '{print $1}' | xargs -I {} tmux kill-session -t {}
+    sleep 1
+    # Preveri, ali so seje res ustavljene
+    if ! tmux list-sessions | grep -q "CCminer\|Update"; then
+        tmux new-session -d -s CCminer
+        tmux send-keys -t CCminer "~/ccminer -c ./config.json" C-m
+        tmux new-session -d -s Update
+        tmux send-keys -t Update "~/ccupdate.sh" C-m
+    else
+        echo -e "\e[91mNapaka pri ustavljanju obstoječih sej!\e[0m"
+        tmux kill-server
+        sleep 2
+        restart_tmux
+    fi
+}
+
 #fi
 # --------------------
 # Check for Stock OS
