af109f37f4b9ac7acd8717b08bd84898260e39f0
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-04-10 21:52:22 +0200

    Update ccupdate.sh

--- 0/ccupdate.sh
+++ 0/ccupdate.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 # v.2025-04-10
 
-# nova verzija screen - tmux  in surr_hash
+# nova verzija screen - tmux in surr_hash
 
-while true; do
-  sleep 99999999
-done
-exit
+# Check for Stock OS or Lineage
+if [[ -z "$(getprop ro.lineage.version)" ]]; then
+    # Stock ROM
+    while true; do; sleep 99999999; done; exit
+else
+
+    iter=1
+    while true; do
+    echo -n -e "\e[96m== $(date '+%Y.%m.%d %H:%M:%S') == ($iter)         \r"
+    # Preverite, ali je trenutna minuta 00 (polna ura)
+    if [[ "$(date +%M)" -eq "00" ]]; then
+        
+    
