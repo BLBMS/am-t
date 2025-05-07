5e0d07bae43c147bac9c8254ab693d8b0ee757e3
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-05-07 23:22:37 +0200

    Update start.sh

--- 0/start.sh
+++ 0/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# v.2025-05-07.001
+# v.2025-05-07.002
 # start 11
 # loči stock rom / lineage  +  tmux + zapiše v all.pools
 # izjemo samo za S8a in S8b
 
-IZJEMA="S8a S8b"
+IZJEME="S8a S8b"
 
 if ! command -v tmux &> /dev/null; then
     echo -e "---nameščam tmux---"
