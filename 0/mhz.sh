c153a7ee2ffe99315228ba3d3d86894037577e90
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-11-04 21:30:53 +0100

    Format CPU frequency output for better readability

--- 0/mhz.sh
+++ 0/mhz.sh
@@ -2,4 +2,7 @@
 # v.2025-11-04
 # by blbMS
 
-for i in {0..7}; do cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_cur_freq 2>/dev/null || echo "CPU$i: Ni podatka"; done
+for i in {0..7}; do
+  echo "CPU$i: $(cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_cur_freq 2>/dev/null || echo "Ni podatka")"
+done
+
